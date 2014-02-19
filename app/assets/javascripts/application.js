c07f96915b567d6737f85008680fd6321d8d4262
Jason Barnabe <jason.barnabe@gmail.com>
2014-02-18 22:28:58 -0600

    Add jquery back - needed for non-GET links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
 
 (function() {
