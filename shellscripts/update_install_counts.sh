a8b91d9adb2cb6c1efb3ff0e77e330666513fb4d
Jason Barnabe <jason.barnabe@gmail.com>
2014-02-18 22:12:53 -0600

    Add logging for update install counts script

--- shellscripts/update_install_counts.sh
+++ shellscripts/update_install_counts.sh
@@ -1,1 +1,3 @@
-~/db < ./update_install_counts.sql >> ./update_install_counts.log
+echo "Starting at `date`" >> ../log/update_install_counts.log
+~/db < ./update_install_counts.sql >> ../log/update_install_counts.log 2>&1
+echo "Done at `date`" >> ../log/update_install_counts.log
