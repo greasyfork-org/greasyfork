7fb1c404eecc25d5318364546c5bdff3fcb661a8
Jason Barnabe <jason.barnabe@gmail.com>
2014-03-15 15:57:55 -0500

    #17 Add logging to auto sync script

--- runnerscripts/auto_sync_scripts.rb
+++ runnerscripts/auto_sync_scripts.rb
@@ -1,6 +1,9 @@
 require 'script_importer/script_syncer'
 
+puts "Started at #{DateTime.now}."
+
 Script.where('script_sync_type_id = 2').where('last_attempted_sync_date < DATE_SUB(UTC_TIMESTAMP(), INTERVAL 1 DAY)').find_each do |script|
 	result = ScriptImporter::ScriptSyncer.sync(script)
 	puts "#{script.id} - #{result} - #{script.sync_error}"
 end
+puts "Completed at #{DateTime.now}."
