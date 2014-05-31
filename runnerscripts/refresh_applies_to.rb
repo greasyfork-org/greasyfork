773ef52a6b4034953930fc42c09b5d8bd5005f12
Jason Barnabe <jason.barnabe@gmail.com>
2014-05-30 21:53:12 -0500

    Remove redundant script

--- runnerscripts/refresh_applies_to.rb
+++
@@ -1,9 +0,0 @@
-applies_tos = {}
-Script.find_each do |script|
-	script.script_applies_tos = script.script_versions.last.calculate_applies_to_names.map do |pattern, name|
-		ScriptAppliesTo.new({:pattern => pattern, :display_text => name})
-	end
-	script.save(:validate => false)
-	puts "Saved #{script.id}"
-end
-
