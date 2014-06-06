b4d81bb04a6c3fa4b90fc3780492ddda3822f9a9
Jason Barnabe <jason.barnabe@gmail.com>
2014-06-06 15:18:00 -0500

    #158 'Assessments script can't be blank' on posting new script

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -2,5 +2,5 @@ class Assessment < ActiveRecord::Base
 	belongs_to :script
 	belongs_to :assessment_reason
 
-	validates_presence_of :script, :assessment_reason
+	validates_presence_of :assessment_reason
 end
