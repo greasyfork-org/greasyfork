437fda7b25f1659334da0020031b0922a51fd4c3
Jason Barnabe <jason.barnabe@gmail.com>
2014-06-25 10:25:54 -0500

    Script to automatically pull from Transifex

---
+++ runnerscripts/update_locales_from_transifex.rb
@@ -0,0 +1,28 @@
+# Log in information:
+# - Uncomment it here (don't uncomment the require line), OR
+# - Put in config/initializers/transifex.rb (including the require line)
+#
+# require 'transifex'
+#
+# Transifex.configure do |config|
+#   config.username = 'your.username'
+#   config.password = 'your.password'
+# end
+
+require 'transifex'
+
+translations_to_download = ['de', 'nl', 'ru', 'zh-CN', 'zh-TW']
+project_slug = 'greasy-fork'
+resource_slug = 'enyml-19'
+
+transifex = Transifex::Client.new
+project = transifex.project(project_slug)
+resource = project.resource(resource_slug)
+translations_to_download.each do |t|
+	File.open("config/locales/#{t}.yml", 'w') { |file|
+		c = resource.translation(t).content
+		# transifex likes underscores in locale names, we like hyphens
+		c.sub!(t.sub('-', '_'), t) if t.include?('-')
+		file.write(c)
+	}
+end
