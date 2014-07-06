cef7dac6713e26e252151f4bb3334973f1fea480
Jason Barnabe <jason.barnabe@gmail.com>
2014-07-05 23:10:09 -0500

    Fix open redirect in to_slug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -131,7 +131,7 @@ protected
 		correct_id = resource.to_param
 		if correct_id != params[id_param_name]
 			params[id_param_name] = correct_id
-			redirect_to params, :status => 301
+			redirect_to params.merge(:only_path => true), :status => 301
 			return true
 		end
 		return false
