69172b61cd7dae9c07b88a6780f6c44ab5f9b882
Jason Barnabe <jason.barnabe@gmail.com>
2014-07-13 08:55:17 -0500

    Set proper max length for script set name and description

--- app/models/script_set.rb
+++ app/models/script_set.rb
@@ -8,6 +8,9 @@ class ScriptSet < ActiveRecord::Base
 	validates_presence_of :name
 	validates_presence_of :description
 
+	validates_length_of :name, :maximum => 100
+	validates_length_of :description, :maximum => 500
+
 	strip_attributes
 
 	def scripts(parents = [])
