39bfc51dc918c2c327ce79f6230a50efee8f787c
Jason Barnabe <jason.barnabe@gmail.com>
2014-08-14 13:58:52 -0500

    #228 Fix path for code dump dir

--- shellscripts/run_cpd.sh
+++ shellscripts/run_cpd.sh
@@ -1,1 +1,1 @@
-../pmd/bin/run.sh cpd --files /www/greasyfork/tmp/cpd --minimum-tokens 100 --language ecmascript --format xml > tmp/cpd/results.xml
+../pmd/bin/run.sh cpd --files tmp/cpd --minimum-tokens 100 --language ecmascript --format xml > tmp/cpd/results.xml
