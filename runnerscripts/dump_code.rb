8bef76a662b951704c74d31acf9bea4d4b14c5c7
Jason Barnabe <jason.barnabe@gmail.com>
2014-08-14 15:18:13 -0500

    #228 Configurable max size for duplicate check

--- runnerscripts/dump_code.rb
+++ runnerscripts/dump_code.rb
@@ -1,6 +1,6 @@
 Script.find_each do |script|
 	code = script.get_newest_saved_script_version.code
-	if code.length < 100000
+	if code.length < Rails.configuration.cpd_size_limit
 		File.open("tmp/cpd/#{script.id}.js", 'w') { |file| file.write(code) }
 	end
 end
