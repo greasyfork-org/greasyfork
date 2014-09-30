6f8dfef9fdd08c52591144c374ce108de905bd2b
Jason Barnabe <jason.barnabe@gmail.com>
2014-09-30 12:02:01 -0500

    redirect_to params not allowed in new Rails #277

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -133,8 +133,7 @@ protected
 		end
 		correct_id = resource.to_param
 		if correct_id != params[id_param_name]
-			params[id_param_name] = correct_id
-			redirect_to params.merge(:only_path => true), :status => 301
+			redirect_to({id_param_name => correct_id}, :status => 301)
 			return true
 		end
 		return false
