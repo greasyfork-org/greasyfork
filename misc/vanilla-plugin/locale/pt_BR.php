317a3018ad7784ab136bb345be96775c70539fcf
Jason Barnabe <jason.barnabe@gmail.com>
2014-11-04 10:59:01 -0600

    Update locales from Transifex

--- misc/vanilla-plugin/locale/pt_BR.php
+++ misc/vanilla-plugin/locale/pt_BR.php
@@ -2,19 +2,19 @@
 /* Label for inputting a script ID when creating/editing a discussion */
 $Definition['Item ID:'] = 'Script ID:';
 // Appended to "Discussions" when viewing a list of discussions on scripts by a user. %s is the user name.
-$Definition['on scripts by %s'] = 'on scripts by %s';
+$Definition['on scripts by %s'] = 'nos scripts por %s';
 /*
  *  Label for the radio buttons for rating a script
  **/
-$Definition['RatingLabel'] = 'How would you rate this script?';
+$Definition['RatingLabel'] = 'Como você avalia este script?';
 # Label for the radio button for not rating a script
-$Definition['RatingNone'] = 'No rating - just a question, comment, feature request, or bug report';
+$Definition['RatingNone'] = 'Sem avaliação - apenas uma pergunta, comentário, pedido ou reportar um erro';
 ## Label for the radio button for reporting a script
-$Definition['RatingReport'] = 'Report script to moderators - malware, stolen code, or other bad things requiring review';
+$Definition['RatingReport'] = 'Reportar script aos moderadores - malware, código roubado ou outras coisas erradas que requerem revisão';
 /** Label for the radio button for rating a script "Bad" */
-$Definition['RatingBad'] = 'Bad - script doesn\'t work';
+$Definition['RatingBad'] = 'Erro - script não funciona';
 /** Label for the radio button for rating a script "OK" **/
-$Definition['RatingOK'] = 'OK - script works, but has significant issues';
+$Definition['RatingOK'] = 'Ok, script funciona mas com erros significantes';
 // Label for the radio button for rating a script "Good"
-$Definition['RatingGood'] = 'Good - script works well';
+$Definition['RatingGood'] = 'Excelente - script funciona bem.';
 
