14604f97a0dbe60c6f7c60b0f2a5f3f3988f0f76
Jason Barnabe <jason.barnabe@gmail.com>
2014-11-03 22:11:49 -0600

    Update locales from Transifex

--- misc/vanilla-plugin/locale/es.php
+++ misc/vanilla-plugin/locale/es.php
@@ -1,10 +1,18 @@
 <?php
-$Definition['Item ID:'] = 'Script ID:';
+// Label for inputting a script ID when creating/editing a discussion
+$Definition['Item ID:'] = 'ID de script';
+// Appended to "Discussions" when viewing a list of discussions on scripts by a user. %s is the user name.
 $Definition['on scripts by %s'] = 'on scripts by %s';
-$Definition['RatingLabel'] = 'How would you rate this script?';
-$Definition['RatingNone'] = 'No rating - just a question, comment, feature request, or bug report';
-$Definition['RatingReport'] = 'Report script to moderators - malware, stolen code, or other bad things requiring review';
-$Definition['RatingBad'] = 'Bad - script doesn\'t work';
-$Definition['RatingOK'] = 'OK - script works, but has significant issues';
-$Definition['RatingGood'] = 'Good - script works well';
+// Label for the radio buttons for rating a script
+$Definition['RatingLabel'] = '¿Cómo calificaría este script?';
+// Label for the radio button for not rating a script
+$Definition['RatingNone'] = 'Ninguna opinión - sólo una pregunta, comentario, solicitud de función, o informe de error';
+// Label for the radio button for reporting a script
+$Definition['RatingReport'] = 'Reportar script a los moderadores - malware, código robado, u otras cosas malas que requieren revisión';
+// Label for the radio button for rating a script "Bad"
+$Definition['RatingBad'] = 'Malo - el script no funciona';
+// Label for the radio button for rating a script "OK"
+$Definition['RatingOK'] = 'OK - el script funciona, pero tiene problemas significativos';
+// Label for the radio button for rating a script "Good"
+$Definition['RatingGood'] = 'Bien - el script funciona bien';
 
--- misc/vanilla-plugin/locale/zh_CN.php
+++ misc/vanilla-plugin/locale/zh_CN.php
@@ -1,10 +1,18 @@
 <?php
+// Label for inputting a script ID when creating/editing a discussion
 $Definition['Item ID:'] = '脚本 ID：';
+// Appended to "Discussions" when viewing a list of discussions on scripts by a user. %s is the user name.
 $Definition['on scripts by %s'] = 'on scripts by %s';
-$Definition['RatingLabel'] = 'How would you rate this script?';
-$Definition['RatingNone'] = 'No rating - just a question, comment, feature request, or bug report';
-$Definition['RatingReport'] = 'Report script to moderators - malware, stolen code, or other bad things requiring review';
-$Definition['RatingBad'] = 'Bad - script doesn\'t work';
-$Definition['RatingOK'] = 'OK - script works, but has significant issues';
-$Definition['RatingGood'] = 'Good - script works well';
+// Label for the radio buttons for rating a script
+$Definition['RatingLabel'] = '您想怎么评价这个脚本？';
+// Label for the radio button for not rating a script
+$Definition['RatingNone'] = '无评分 - 只是一个询问，评论，功能要求，或错误报告';
+// Label for the radio button for reporting a script
+$Definition['RatingReport'] = '向管理员举报脚本 - 恶意代码、抄袭代码或其它需要审核的脚本';
+// Label for the radio button for rating a script "Bad"
+$Definition['RatingBad'] = '不好 - 脚本不起作用';
+// Label for the radio button for rating a script "OK"
+$Definition['RatingOK'] = '还好 - 脚本起作用，但有显著的问题';
+// Label for the radio button for rating a script "Good"
+$Definition['RatingGood'] = '非常好 - 脚本运行良好';
 
