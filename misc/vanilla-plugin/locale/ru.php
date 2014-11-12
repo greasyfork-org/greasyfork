e7e2862e10cb9185d07001c6cee112e22252dd36
Jason Barnabe <jason.barnabe@gmail.com>
2014-11-12 09:38:07 -0600

    Update locales from Transifex

--- misc/vanilla-plugin/locale/ru.php
+++ misc/vanilla-plugin/locale/ru.php
@@ -32,10 +32,10 @@ $Definition['Request someone write a script for you.'] = 'Вы можете по
 // Category name
 $Definition['Script Discussions'] = 'Обсуждение скриптов';
 // Category description for Script Discussions
-$Definition['Discussion on scripts posted to Greasy Fork.'] = 'Обсуждение работы скриптов, размещенных на Greasy Fork.';
+$Definition['Discussion on scripts posted to Greasy Fork.'] = 'Обсуждение скриптов, размещенных на Greasy Fork.';
 // Link on Recent Discussions to hide script discussions
-$Definition['Turn on filter'] = 'Hide script discussions';
+$Definition['Turn on filter'] = 'Скрыть обсуждения скрипта';
 // Link on Recent Discussions to show script discussions
-$Definition['Turn off filter'] = 'Show script discussions';
+$Definition['Turn off filter'] = 'Показать обсуждения скрипта';
 // Link on forum profile pointing to main site profile
-$Definition['Greasy Fork Profile'] = 'Greasy Fork Profile';
+$Definition['Greasy Fork Profile'] = 'Профиль Greasy Fork';
--- misc/vanilla-theme/locale/ja.php
+++ misc/vanilla-theme/locale/ja.php
@@ -1,5 +1,5 @@
 <?php
-$Definition['layouts.application.script_list'] = 'スクリプト一覧';
+$Definition['layouts.application.script_list'] = 'スクリプト';
 $Definition['layouts.application.forum'] = 'フォーラム';
 $Definition['layouts.application.help'] = 'ヘルプ';
 $Definition['layouts.application.search'] = '検索';
