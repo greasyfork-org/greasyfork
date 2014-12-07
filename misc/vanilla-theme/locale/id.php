28c904ac3ab58423790cac8162ec729b04cee993
Jason Barnabe <jason.barnabe@gmail.com>
2014-12-07 14:58:05 -0600

    Update locales from Transifex

--- misc/vanilla-theme/locale/id.php
+++ misc/vanilla-theme/locale/id.php
@@ -1,5 +1,5 @@
 <?php
-$Definition['layouts.application.script_list'] = 'Naskah';
+$Definition['layouts.application.script_list'] = 'Skrip';
 $Definition['layouts.application.forum'] = 'Forum';
 $Definition['layouts.application.help'] = 'Bantuan';
 $Definition['layouts.application.search'] = 'Cari';
