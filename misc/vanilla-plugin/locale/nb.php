dbd19fe667221ad0bd9ad86dd4f193538c626989
Jason Barnabe <jason.barnabe@gmail.com>
2014-12-23 12:42:39 -0600

    Update locales from Transifex

---
+++ misc/vanilla-plugin/locale/nb.php
@@ -0,0 +1,43 @@
+<?php
+/* Label for inputting a script ID when creating/editing a discussion */
+$Definition['Item ID:'] = 'Script ID:';
+// Appended to "Discussions" when viewing a list of discussions on scripts by a user. %s is the user name.
+$Definition['on scripts by %s'] = 'on scripts by %s';
+/*
+ *  Label for the radio buttons for rating a script
+ **/
+$Definition['RatingLabel'] = 'How would you rate this script?';
+# Label for the radio buttons for updating a rating on a script
+$Definition['RatingUpdateLabel'] = 'Update your rating:';
+# Label for the radio button for not rating a script
+$Definition['RatingNone'] = 'No rating - just a question, comment, feature request, or bug report';
+## Label for the radio button for reporting a script
+$Definition['RatingReport'] = 'Report script to moderators for removal - malware, stolen code, etc.';
+/** Label for the radio button for rating a script "Bad" */
+$Definition['RatingBad'] = 'Bad - script doesn\'t work';
+/** Label for the radio button for rating a script "OK" **/
+$Definition['RatingOK'] = 'OK - script works, but has significant issues';
+// Label for the radio button for rating a script "Good"
+$Definition['RatingGood'] = 'Good - script works well';
+// Category name
+$Definition['Site Feedback'] = 'Site Feedback';
+// Category description for Site Feedback
+$Definition['Bug reports, feature requests, and other stuff about Greasy Fork itself.'] = 'Bug reports, feature requests, and other stuff about Greasy Fork itself.';
+// Category name
+$Definition['Development'] = 'Development';
+// Category description for Development
+$Definition['Technical talk about user script development.'] = 'Technical talk about user script development.';
+// Category name
+$Definition['Script Creation Requests'] = 'Script Creation Requests';
+// Category description for Script Creation Requests
+$Definition['Request someone write a script for you.'] = 'Request someone write a script for you.';
+// Category name
+$Definition['Script Discussions'] = 'Script Discussions';
+// Category description for Script Discussions
+$Definition['Discussion on scripts posted to Greasy Fork.'] = 'Discussion on scripts posted to Greasy Fork.';
+// Link on Recent Discussions to hide script discussions
+$Definition['Turn on filter'] = 'Hide script discussions';
+// Link on Recent Discussions to show script discussions
+$Definition['Turn off filter'] = 'Show script discussions';
+// Link on forum profile pointing to main site profile
+$Definition['Greasy Fork Profile'] = 'Greasy Fork Profile';
