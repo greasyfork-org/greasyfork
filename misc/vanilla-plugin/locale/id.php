87bb1544909d2e8daa78cd73136b534811c72a1c
Jason Barnabe <jason.barnabe@gmail.com>
2015-02-17 11:44:06 -0600

    Update locales from Transifex

--- misc/vanilla-plugin/locale/id.php
+++ misc/vanilla-plugin/locale/id.php
@@ -1,43 +1,43 @@
 <?php
 /* Label for inputting a script ID when creating/editing a discussion */
-$Definition['Item ID:'] = 'ID Naskah:';
+$Definition['Item ID:'] = 'ID Skrip:';
 // Appended to "Discussions" when viewing a list of discussions on scripts by a user. %s is the user name.
-$Definition['on scripts by %s'] = 'pada naskah oleh %s';
+$Definition['on scripts by %s'] = 'pada skrip oleh %s';
 /*
  *  Label for the radio buttons for rating a script
  **/
-$Definition['RatingLabel'] = 'Bagaimana anda menilai naskah ini?';
+$Definition['RatingLabel'] = 'Bagaimana anda menilai skrip ini?';
 # Label for the radio buttons for updating a rating on a script
 $Definition['RatingUpdateLabel'] = 'Perbarui nilai anda:';
 # Label for the radio button for not rating a script
-$Definition['RatingNone'] = 'Bukan penilaian - hanya pertanyaan, komentar, permintaan fitur, atau laporan bug';
+$Definition['RatingNone'] = 'Tanpa penilaian - hanya pertanyaan, komentar, permintaan fitur, atau laporan bug';
 ## Label for the radio button for reporting a script
-$Definition['RatingReport'] = 'Laporkan naskah ke moderator untuk dihapus - malware, pencurian kode, dll.';
+$Definition['RatingReport'] = 'Laporkan skrip ke moderator untuk dihapus - malware, pencurian kode, dll.';
 /** Label for the radio button for rating a script "Bad" */
-$Definition['RatingBad'] = 'Buruk - naskah tidak bekerja';
+$Definition['RatingBad'] = 'Buruk - skrip tidak bekerja';
 /** Label for the radio button for rating a script "OK" **/
-$Definition['RatingOK'] = 'OK - naskah bekerja, tapi memiliki masalah signifikan';
+$Definition['RatingOK'] = 'OK - skrip bekerja, tapi memiliki masalah yang signifikan';
 // Label for the radio button for rating a script "Good"
-$Definition['RatingGood'] = 'Bagus - naskah bekerja dengan baik';
+$Definition['RatingGood'] = 'Bagus - skrip bekerja dengan baik';
 // Category name for Site Feedback
-$Definition['Categories.general.Name'] = 'Site Feedback';
+$Definition['Categories.general.Name'] = 'Umpan Balik Situs';
 // Category description for Site Feedback
-$Definition['Categories.general.Description'] = 'Bug reports, feature requests, and other stuff about Greasy Fork itself.';
+$Definition['Categories.general.Description'] = 'Laporan bug, permintaan fitur, dan hal lain mengenai Greasy Fork itu sendiri.';
 // Category name for Development
-$Definition['Categories.script-development.Name'] = 'Development';
+$Definition['Categories.script-development.Name'] = 'Pengembangan';
 // Category description for Development
-$Definition['Categories.script-development.Description'] = 'Technical talk about user script development.';
+$Definition['Categories.script-development.Description'] = 'Pembicaraan teknis mengenai pengembangan skrip pengguna.';
 // Category name for Script Creation Requests
-$Definition['Categories.script-requests.Name'] = 'Script Creation Requests';
+$Definition['Categories.script-requests.Name'] = 'Permintaan Pembuatan Skrip';
 // Category description for Script Creation Requests
-$Definition['Categories.script-requests.Description'] = 'Request someone write a script for you.';
+$Definition['Categories.script-requests.Description'] = 'Minta seseorang menuliskan sebuah skrip untukmu.';
 // Category name for Script Discussions
-$Definition['Categories.script-discussions.Name'] = 'Script Discussions';
+$Definition['Categories.script-discussions.Name'] = 'Diskusi Skrip';
 // Category description for Script Discussions
-$Definition['Categories.script-discussions.Description'] = 'Discussion on scripts posted to Greasy Fork.';
+$Definition['Categories.script-discussions.Description'] = 'Diskusi pada skrip yang diposting ke Greasy Fork.';
 // Link on Recent Discussions to hide script discussions
-$Definition['Turn on filter'] = 'Sembunyikan diskusi naskah';
+$Definition['Turn on filter'] = 'Sembunyikan diskusi skrip';
 // Link on Recent Discussions to show script discussions
-$Definition['Turn off filter'] = 'Tampilkan diskusi naskah';
+$Definition['Turn off filter'] = 'Tampilkan diskusi skrip';
 // Link on forum profile pointing to main site profile
 $Definition['Greasy Fork Profile'] = 'Profil Greasy Fork';
