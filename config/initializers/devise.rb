9922baed2811cb5f7dba27d55d4e3969d6c9cdc9
Jason <jason.barnabe@gmail.com>
2015-03-13 15:32:42 -0500

    Move secrets to secrets.yml

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,11 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  # The secret key used by Devise. Devise uses this key to generate
+  # random tokens. Changing this key will render invalid all existing
+  # confirmation, reset password and unlock tokens in the database.
+  config.secret_key = Rails.application.secrets.devise_secret_key
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
--- test/travisconfig/devise_secret.rb
+++
@@ -1,4 +0,0 @@
-Devise.setup do |config|
-  # This value is public, don't use it for reals!
-  config.secret_key = 'da9215548e04d91d634e4a69f270e75aa6dd1ea07962002f21b1a0cf5b7111abbb2727ea120631af25b7352bb9dc6695fd9ddd7c7a478af8faf20ebc9ea059fc'
-end
