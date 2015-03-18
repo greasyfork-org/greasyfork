05c78424452034e59891c4a510260e5de6681537
Jason <jason.barnabe@gmail.com>
2015-03-18 13:16:28 -0500

    Upgrade to Ruby 2.2.1

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '2.1.3'
+ruby '2.2.1'
 
 gem 'rails', '~> 4.2.0'
 gem 'rails-i18n', '~> 4.0.0'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,10 @@ Greasyfork::Application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  #config.active_support.deprecation = :log
+  config.active_support.deprecation = Proc.new { |message, callstack|
+    raise NameError, message, callstack
+  }
 
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
