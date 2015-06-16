ba8089ac9a4645a6c58ad0a482de614a1a55bfe3
Jason <jason.barnabe@gmail.com>
2015-06-15 20:48:35 -0500

    Update locales from Transifex

--- misc/vanilla-plugin/locale/hu.php
+++ misc/vanilla-plugin/locale/hu.php
@@ -1,30 +1,30 @@
 <?php
 /* Label for inputting a script ID when creating/editing a discussion */
-$Definition['Item ID:'] = 'Script ID:';
+$Definition['Item ID:'] = 'Szkipt azonosító:';
 // Appended to "Discussions" when viewing a list of discussions on scripts by a user. %s is the user name.
 $Definition['on scripts by %s'] = 'on scripts by %s';
 /*
  *  Label for the radio buttons for rating a script
  **/
-$Definition['RatingLabel'] = 'How would you rate this script?';
+$Definition['RatingLabel'] = 'Hogyan értékelné ezt a szkriptet?';
 # Label for the radio buttons for updating a rating on a script
-$Definition['RatingUpdateLabel'] = 'Update your rating:';
+$Definition['RatingUpdateLabel'] = 'Frissítse az értékelését';
 # Label for the radio button for not rating a script
-$Definition['RatingNone'] = 'No rating - just a question, comment, feature request, or bug report';
+$Definition['RatingNone'] = 'Nincs értékelés - csak kérdés, komment, feature kérése, vagy hiba jelentés';
 ## Label for the radio button for reporting a script
 $Definition['RatingReport'] = 'Report script to moderators for removal - malware, stolen code, etc.';
 /** Label for the radio button for rating a script "Bad" */
-$Definition['RatingBad'] = 'Bad - script doesn\'t work';
+$Definition['RatingBad'] = 'Rossz - a szkript nem működik';
 /** Label for the radio button for rating a script "OK" **/
-$Definition['RatingOK'] = 'OK - script works, but has significant issues';
+$Definition['RatingOK'] = 'Oké - a szkript működik, de jelentős problémái vannak';
 // Label for the radio button for rating a script "Good"
 $Definition['RatingGood'] = 'Good - script works well';
 // Category name for Site Feedback
-$Definition['Categories.general.Name'] = 'Site Feedback';
+$Definition['Categories.general.Name'] = 'Weboldal Visszajelzés';
 // Category description for Site Feedback
 $Definition['Categories.general.Description'] = 'Bug reports, feature requests, and other stuff about Greasy Fork itself.';
 // Category name for Development
-$Definition['Categories.script-development.Name'] = 'Development';
+$Definition['Categories.script-development.Name'] = 'Fejlesztés';
 // Category description for Development
 $Definition['Categories.script-development.Description'] = 'Technical talk about user script development.';
 // Category name for Script Creation Requests
@@ -32,12 +32,12 @@ $Definition['Categories.script-requests.Name'] = 'Script Creation Requests';
 // Category description for Script Creation Requests
 $Definition['Categories.script-requests.Description'] = 'Request someone write a script for you.';
 // Category name for Script Discussions
-$Definition['Categories.script-discussions.Name'] = 'Script Discussions';
+$Definition['Categories.script-discussions.Name'] = 'Szkript Megbeszélések';
 // Category description for Script Discussions
 $Definition['Categories.script-discussions.Description'] = 'Discussion on scripts posted to Greasy Fork.';
 // Link on Recent Discussions to hide script discussions
 $Definition['Turn on filter'] = 'Hide script discussions';
 // Link on Recent Discussions to show script discussions
-$Definition['Turn off filter'] = 'Show script discussions';
+$Definition['Turn off filter'] = 'Szkript vita mutatása';
 // Link on forum profile pointing to main site profile
 $Definition['Greasy Fork Profile'] = 'Greasy Fork Profile';
--- misc/vanilla-plugin/locale/sv.php
+++ misc/vanilla-plugin/locale/sv.php
@@ -1,6 +1,6 @@
 <?php
 /* Label for inputting a script ID when creating/editing a discussion */
-$Definition['Item ID:'] = 'Script ID:';
+$Definition['Item ID:'] = 'Skript-ID:';
 // Appended to "Discussions" when viewing a list of discussions on scripts by a user. %s is the user name.
 $Definition['on scripts by %s'] = 'on scripts by %s';
 /*
@@ -10,34 +10,34 @@ $Definition['RatingLabel'] = 'Hur skulle du betygsätta detta skript?';
 # Label for the radio buttons for updating a rating on a script
 $Definition['RatingUpdateLabel'] = 'Uppdatera ditt betyg:';
 # Label for the radio button for not rating a script
-$Definition['RatingNone'] = 'ingen betygsättning - Bara en fråga, kommentera, funktionsförfrågan eller felrapport';
+$Definition['RatingNone'] = 'ingen betygsättning - Bara en fråga, kommentar, funktionsförfrågan eller felrapport';
 ## Label for the radio button for reporting a script
-$Definition['RatingReport'] = 'apportera skript ';
+$Definition['RatingReport'] = 'Rapportera skript ';
 /** Label for the radio button for rating a script "Bad" */
 $Definition['RatingBad'] = 'dåligt - skriptet fungerar inte';
 /** Label for the radio button for rating a script "OK" **/
 $Definition['RatingOK'] = 'OK - skript fungerar, men har några betydelsefulla problem';
 // Label for the radio button for rating a script "Good"
-$Definition['RatingGood'] = 'bra - scriptet funkar bra';
+$Definition['RatingGood'] = 'Bra - skriptet funkar bra';
 // Category name for Site Feedback
 $Definition['Categories.general.Name'] = 'sido-Feedback';
 // Category description for Site Feedback
-$Definition['Categories.general.Description'] = 'Bugg rapporter, funktions önskningar, och andra saker om Greasy Fork själv';
+$Definition['Categories.general.Description'] = 'Buggrapporter, funktionsförfrågningar, och andra saker om Greasy Fork själv';
 // Category name for Development
-$Definition['Categories.script-development.Name'] = 'utveckling';
+$Definition['Categories.script-development.Name'] = 'Utveckling';
 // Category description for Development
-$Definition['Categories.script-development.Description'] = 'Tekniskt prat om användarskripts-utvekling.';
+$Definition['Categories.script-development.Description'] = 'Tekniskt prat om användarskripts-utveckling.';
 // Category name for Script Creation Requests
-$Definition['Categories.script-requests.Name'] = 'Skript skap';
+$Definition['Categories.script-requests.Name'] = 'Script Creation Requests';
 // Category description for Script Creation Requests
 $Definition['Categories.script-requests.Description'] = 'Request someone write a script for you.';
 // Category name for Script Discussions
-$Definition['Categories.script-discussions.Name'] = 'Skript diskition';
+$Definition['Categories.script-discussions.Name'] = 'Skript-diskussioner';
 // Category description for Script Discussions
 $Definition['Categories.script-discussions.Description'] = 'Discussion on scripts posted to Greasy Fork.';
 // Link on Recent Discussions to hide script discussions
-$Definition['Turn on filter'] = 'Göm skript diskution';
+$Definition['Turn on filter'] = 'Göm skript-diskussion';
 // Link on Recent Discussions to show script discussions
-$Definition['Turn off filter'] = 'Visa skript diskussion ';
+$Definition['Turn off filter'] = 'Visa skript-diskussion ';
 // Link on forum profile pointing to main site profile
-$Definition['Greasy Fork Profile'] = 'Greasy Fork profil';
+$Definition['Greasy Fork Profile'] = 'Greasy Fork-profil';
