33f238c2dfb5eaba20c8d133556b482f32bfe706
Jason <jason.barnabe@gmail.com>
2015-06-16 22:39:16 -0500

    Update locales from Transifex

--- misc/vanilla-plugin/locale/hu.php
+++ misc/vanilla-plugin/locale/hu.php
@@ -1,6 +1,6 @@
 <?php
 /* Label for inputting a script ID when creating/editing a discussion */
-$Definition['Item ID:'] = 'Szkipt azonosító:';
+$Definition['Item ID:'] = 'Szkript azonosító:';
 // Appended to "Discussions" when viewing a list of discussions on scripts by a user. %s is the user name.
 $Definition['on scripts by %s'] = 'on scripts by %s';
 /*
@@ -10,19 +10,19 @@ $Definition['RatingLabel'] = 'Hogyan értékelné ezt a szkriptet?';
 # Label for the radio buttons for updating a rating on a script
 $Definition['RatingUpdateLabel'] = 'Frissítse az értékelését';
 # Label for the radio button for not rating a script
-$Definition['RatingNone'] = 'Nincs értékelés - csak kérdés, komment, feature kérése, vagy hiba jelentés';
+$Definition['RatingNone'] = 'Nincs értékelés - csak kérdés, komment, feature kérése vagy hiba jelentése';
 ## Label for the radio button for reporting a script
-$Definition['RatingReport'] = 'Report script to moderators for removal - malware, stolen code, etc.';
+$Definition['RatingReport'] = 'Szkript jelentése moderátoroknak eltávolításhoz - malware, lopott kód, stb.';
 /** Label for the radio button for rating a script "Bad" */
 $Definition['RatingBad'] = 'Rossz - a szkript nem működik';
 /** Label for the radio button for rating a script "OK" **/
 $Definition['RatingOK'] = 'Oké - a szkript működik, de jelentős problémái vannak';
 // Label for the radio button for rating a script "Good"
-$Definition['RatingGood'] = 'Good - script works well';
+$Definition['RatingGood'] = 'Jó - a szkript jól működik';
 // Category name for Site Feedback
-$Definition['Categories.general.Name'] = 'Weboldal Visszajelzés';
+$Definition['Categories.general.Name'] = 'Visszajelzés';
 // Category description for Site Feedback
-$Definition['Categories.general.Description'] = 'Bug reports, feature requests, and other stuff about Greasy Fork itself.';
+$Definition['Categories.general.Description'] = 'Hibajelentések, feature kérések és egyéb dolgok a Greasy Forkkal kapcsolatban';
 // Category name for Development
 $Definition['Categories.script-development.Name'] = 'Fejlesztés';
 // Category description for Development
@@ -30,14 +30,14 @@ $Definition['Categories.script-development.Description'] = 'Technical talk about
 // Category name for Script Creation Requests
 $Definition['Categories.script-requests.Name'] = 'Script Creation Requests';
 // Category description for Script Creation Requests
-$Definition['Categories.script-requests.Description'] = 'Request someone write a script for you.';
+$Definition['Categories.script-requests.Description'] = 'Kérj meg valakit, hogy írjon neked egy szkriptet.';
 // Category name for Script Discussions
-$Definition['Categories.script-discussions.Name'] = 'Szkript Megbeszélések';
+$Definition['Categories.script-discussions.Name'] = 'Szkript megbeszélések';
 // Category description for Script Discussions
 $Definition['Categories.script-discussions.Description'] = 'Discussion on scripts posted to Greasy Fork.';
 // Link on Recent Discussions to hide script discussions
-$Definition['Turn on filter'] = 'Hide script discussions';
+$Definition['Turn on filter'] = 'Szkript vita elrejtése';
 // Link on Recent Discussions to show script discussions
 $Definition['Turn off filter'] = 'Szkript vita mutatása';
 // Link on forum profile pointing to main site profile
-$Definition['Greasy Fork Profile'] = 'Greasy Fork Profile';
+$Definition['Greasy Fork Profile'] = 'Greasy Fork profil';
--- misc/vanilla-theme/locale/hu.php
+++ misc/vanilla-theme/locale/hu.php
@@ -1,5 +1,5 @@
 <?php
 $Definition['layouts.application.script_list'] = 'Szkriptek';
-$Definition['layouts.application.forum'] = 'Forum';
+$Definition['layouts.application.forum'] = 'Fórum';
 $Definition['layouts.application.help'] = 'Segítség';
 $Definition['layouts.application.search'] = 'Keresés';
