ca94a2affa2522892222e743e06f698222531b80
Jason <jason.barnabe@gmail.com>
2015-06-26 22:44:25 -0500

    Update locales from Transifex

--- misc/vanilla-plugin/locale/pl.php
+++ misc/vanilla-plugin/locale/pl.php
@@ -1,43 +1,43 @@
 <?php
 /* Label for inputting a script ID when creating/editing a discussion */
-$Definition['Item ID:'] = 'ID skryptu:';
+$Definition['Item ID:'] = 'Identyfikator skryptu:';
 // Appended to "Discussions" when viewing a list of discussions on scripts by a user. %s is the user name.
-$Definition['on scripts by %s'] = 'na temat skryptów od %s';
+$Definition['on scripts by %s'] = 'o skryptach od %s';
 /*
  *  Label for the radio buttons for rating a script
  **/
-$Definition['RatingLabel'] = 'Jaką ocenę chcesz wystawić temu skryptowi?';
+$Definition['RatingLabel'] = 'Jak oceniasz ten skrypt?';
 # Label for the radio buttons for updating a rating on a script
 $Definition['RatingUpdateLabel'] = 'Zaktualizuj swoją ocenę:';
 # Label for the radio button for not rating a script
 $Definition['RatingNone'] = 'Brak ocen - tylko pytanie, komentarz, propozycja funkcji lub zgłoszenie błędu';
 ## Label for the radio button for reporting a script
-$Definition['RatingReport'] = 'Zgłoś skrypt do usunięcia - złośliwe oprogramowanie, skradziony kod, itp.';
+$Definition['RatingReport'] = 'Zgłoś skrypt moderatorom do usunięcia - złośliwe oprogramowanie, ukradziony kod, itp.';
 /** Label for the radio button for rating a script "Bad" */
 $Definition['RatingBad'] = 'Źle - skrypt nie działa';
 /** Label for the radio button for rating a script "OK" **/
-$Definition['RatingOK'] = 'Okej - skrypt działa, ale ma poważne problemy';
+$Definition['RatingOK'] = 'Okej - skrypt działa, ale ma istotne problemy';
 // Label for the radio button for rating a script "Good"
-$Definition['RatingGood'] = 'Dobrze - skrypt działa odpowiednio';
+$Definition['RatingGood'] = 'Dobrze - skrypt działa poprawnie';
 // Category name for Site Feedback
 $Definition['Categories.general.Name'] = 'Opinia o stronie';
 // Category description for Site Feedback
-$Definition['Categories.general.Description'] = 'Zgłoszenie błędów, propozycje nowych funkcji i inne rzeczy na temat Greasy Fork.';
+$Definition['Categories.general.Description'] = 'Zgłoszenie błędów, propozycje funkcji i inne rzeczy na Greasy Fork.';
 // Category name for Development
 $Definition['Categories.script-development.Name'] = 'Rozwój';
 // Category description for Development
-$Definition['Categories.script-development.Description'] = 'Rozmowy techniczne na temat rozwoju skryptu.';
+$Definition['Categories.script-development.Description'] = 'Rozmowy techniczne o rozwoju skryptu użytkownika.';
 // Category name for Script Creation Requests
-$Definition['Categories.script-requests.Name'] = 'Propozycje skryptu';
+$Definition['Categories.script-requests.Name'] = 'Propozycje stworzenia skryptu';
 // Category description for Script Creation Requests
 $Definition['Categories.script-requests.Description'] = 'Poproś o napisanie skryptu dla Ciebie.';
 // Category name for Script Discussions
-$Definition['Categories.script-discussions.Name'] = 'Dyskusje na temat skryptu';
+$Definition['Categories.script-discussions.Name'] = 'Dyskusje o skrypcie';
 // Category description for Script Discussions
-$Definition['Categories.script-discussions.Description'] = 'Dyskusje na temat skryptów opublikowanych na Greasy Fork.';
+$Definition['Categories.script-discussions.Description'] = 'Dyskusje o skryptach udostępnionych na Greasy Fork.';
 // Link on Recent Discussions to hide script discussions
-$Definition['Turn on filter'] = 'Ukryj dyskusję na temat tego skryptu';
+$Definition['Turn on filter'] = 'Ukryj dyskusję o tym skrypcie';
 // Link on Recent Discussions to show script discussions
-$Definition['Turn off filter'] = 'Pokaż dyskusję na temat tego skryptu';
+$Definition['Turn off filter'] = 'Pokaż dyskusję o tym skrypcie';
 // Link on forum profile pointing to main site profile
 $Definition['Greasy Fork Profile'] = 'Profil na Greasy Fork';
