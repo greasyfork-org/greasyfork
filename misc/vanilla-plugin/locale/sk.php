6109a300872143752c13f538337eeaaa6e50836c
Jason <jason.barnabe@gmail.com>
2016-02-17 21:26:04 -0600

    Update locales from Transifex

--- misc/vanilla-plugin/locale/sk.php
+++ misc/vanilla-plugin/locale/sk.php
@@ -2,13 +2,13 @@
 /* Label for inputting a script ID when creating/editing a discussion */
 $Definition['Item ID:'] = 'ID skriptu:';
 // Appended to "Discussions" when viewing a list of discussions on scripts by a user. %s is the user name.
-$Definition['on scripts by %s'] = 'pri skripte od %s';
+$Definition['on scripts by %s'] = 'o skripte od %s';
 /*
  *  Label for the radio buttons for rating a script
  **/
 $Definition['RatingLabel'] = 'Ako by ste ohodnotili tento skript?';
 # Label for the radio buttons for updating a rating on a script
-$Definition['RatingUpdateLabel'] = 'Aktualizujte svoje hodnotenie:';
+$Definition['RatingUpdateLabel'] = 'Aktualizovať svoje hodnotenie:';
 # Label for the radio button for not rating a script
 $Definition['RatingNone'] = 'Žiadne hodnotenie - len otázka, komentár, žiadosť o funkciu, alebo nahlásenie chyby';
 ## Label for the radio button for reporting a script
@@ -22,22 +22,22 @@ $Definition['RatingGood'] = 'Dobré - skript funguje dobre';
 // Category name for Site Feedback
 $Definition['Categories.general.Name'] = 'Spätná vazba na stránku';
 // Category description for Site Feedback
-$Definition['Categories.general.Description'] = 'Hlásenie chýb, žiadosti o funkcie a ďalšie veci ohľadom Greasy Fork.';
+$Definition['Categories.general.Description'] = 'Hlásenie chýb, žiadosti o funkcie a ostatné veci ohľadom Greasy Fork.';
 // Category name for Development
 $Definition['Categories.script-development.Name'] = 'Vývoj';
 // Category description for Development
-$Definition['Categories.script-development.Description'] = 'Technická diskusia o vývoji užívateľskom skripte.';
+$Definition['Categories.script-development.Description'] = 'Technická diskusia o vývoji užívateľského skriptu.';
 // Category name for Script Creation Requests
 $Definition['Categories.script-requests.Name'] = 'Požiadavka na vytvorenie skriptu';
 // Category description for Script Creation Requests
-$Definition['Categories.script-requests.Description'] = 'Požiadať niekoho na napísanie skriptu pre Vás.';
+$Definition['Categories.script-requests.Description'] = 'Požiadať niekoho, aby vám napísal skript.';
 // Category name for Script Discussions
-$Definition['Categories.script-discussions.Name'] = 'Diskusia k skriptu';
+$Definition['Categories.script-discussions.Name'] = 'Diskusia o skripte';
 // Category description for Script Discussions
 $Definition['Categories.script-discussions.Description'] = 'Diskusia o skriptoch odoslaných na Greasy Fork';
 // Link on Recent Discussions to hide script discussions
-$Definition['Turn on filter'] = 'Schovať diskusiu k skriptu';
+$Definition['Turn on filter'] = 'Schovať diskusiu o skripte';
 // Link on Recent Discussions to show script discussions
-$Definition['Turn off filter'] = 'Zobraziť diskusiu k skriptu';
+$Definition['Turn off filter'] = 'Zobraziť diskusiu o skripte';
 // Link on forum profile pointing to main site profile
 $Definition['Greasy Fork Profile'] = 'Zobraziť profil na Greasy Fork';
