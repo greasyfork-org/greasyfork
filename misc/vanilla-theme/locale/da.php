32e834e7508861b18c489b0b4558389a6e17c5ae
Jason <jason.barnabe@gmail.com>
2016-06-08 18:44:47 -0500

    Update locales from Transifex

--- misc/vanilla-theme/locale/da.php
+++ misc/vanilla-theme/locale/da.php
@@ -1,5 +1,5 @@
 <?php
-$Definition['layouts.application.script_list'] = 'Scripts';
+$Definition['layouts.application.script_list'] = 'Script ID:';
 $Definition['layouts.application.forum'] = 'Forum';
 $Definition['layouts.application.help'] = 'Hjælp';
 $Definition['layouts.application.search'] = 'Søg';
