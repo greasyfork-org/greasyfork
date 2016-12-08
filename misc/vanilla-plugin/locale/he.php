58190faf5c3cebc8e468875be08c5fe4b0aa7f90
Jason <jason.barnabe@gmail.com>
2016-12-07 21:27:42 -0600

    Update locales from Transifex

---
+++ misc/vanilla-plugin/locale/he.php
@@ -0,0 +1,43 @@
+<?php
+/* Label for inputting a script ID when creating/editing a discussion */
+$Definition['Item ID:'] = 'Script ID:';
+// Appended to "Discussions" when viewing a list of discussions on scripts by a user. %s is the user name.
+$Definition['on scripts by %s'] = 'on scripts by %s';
+/*
+ *  Label for the radio buttons for rating a script
+ **/
+$Definition['RatingLabel'] = 'דרג את הסקריפט';
+# Label for the radio buttons for updating a rating on a script
+$Definition['RatingUpdateLabel'] = 'עדכן את הדירוג';
+# Label for the radio button for not rating a script
+$Definition['RatingNone'] = 'ללא דירוג- רק שאלה, הערה, בקשה להוספת פיצ\'ר או דיווח על תקלה';
+## Label for the radio button for reporting a script
+$Definition['RatingReport'] = 'Report script to moderators for removal - malware, stolen code, etc.';
+/** Label for the radio button for rating a script "Bad" */
+$Definition['RatingBad'] = 'לא טוב- הסקריפט אינו עובד';
+/** Label for the radio button for rating a script "OK" **/
+$Definition['RatingOK'] = 'בסדר- הסקריפט עובד, אך סובל מבעיות מסויימות';
+// Label for the radio button for rating a script "Good"
+$Definition['RatingGood'] = 'מעולה- הסקירפט עובד באופן תקין';
+// Category name for Site Feedback
+$Definition['Categories.general.Name'] = 'Site Feedback';
+// Category description for Site Feedback
+$Definition['Categories.general.Description'] = 'Bug reports, feature requests, and other stuff about Greasy Fork itself.';
+// Category name for Development
+$Definition['Categories.script-development.Name'] = 'פיתוח';
+// Category description for Development
+$Definition['Categories.script-development.Description'] = 'Technical talk about user script development.';
+// Category name for Script Creation Requests
+$Definition['Categories.script-requests.Name'] = 'בקשות ליצירת סקריפט';
+// Category description for Script Creation Requests
+$Definition['Categories.script-requests.Description'] = 'בקש יצירת סקריפט עבורך';
+// Category name for Script Discussions
+$Definition['Categories.script-discussions.Name'] = 'Script Discussions';
+// Category description for Script Discussions
+$Definition['Categories.script-discussions.Description'] = 'Discussion on scripts posted to Greasy Fork.';
+// Link on Recent Discussions to hide script discussions
+$Definition['Turn on filter'] = 'Hide script discussions';
+// Link on Recent Discussions to show script discussions
+$Definition['Turn off filter'] = 'Show script discussions';
+// Link on forum profile pointing to main site profile
+$Definition['Greasy Fork Profile'] = 'פרופיל greesy fork';
