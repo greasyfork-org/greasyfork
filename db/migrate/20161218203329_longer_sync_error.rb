cd3d4ff5e3ada5c52acf76e283263391a27e7101
Jason <jason.barnabe@gmail.com>
2016-12-18 14:42:15 -0600

    Allow longer sync errors to be recorded, attempt 2

--- db/migrate/20161218203329_longer_sync_error.rb
+++ db/migrate/20161218203329_longer_sync_error.rb
@@ -1,5 +1,5 @@
 class LongerSyncError < ActiveRecord::Migration[5.0]
   def change
-    change_column :scripts, :sync_error, :string, length: 1000
+    change_column :scripts, :sync_error, :string, limit: 1000
   end
 end
