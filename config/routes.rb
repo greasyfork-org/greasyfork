faac1fe0dd11d9166ec45dc2c109e46a8d7d49df
Jason <jason.barnabe@gmail.com>
2016-12-26 12:17:46 -0600

    Remove removed route

--- config/routes.rb
+++ config/routes.rb
@@ -74,7 +74,6 @@ Greasyfork::Application.routes.draw do
 				get 'by-site(.:format)', :action => 'by_site', :as => 'site_list'
 				# :site can contain a dot, make sure site doesn't eat format or vice versa
 				get 'by-site/:site(.:format)', :action => 'index', :as => 'by_site', :constraints => {:site => /.*?/, :format => /|html|atom|json|jsonp/}
-				get 'under-assessment(.:format)', :action => 'under_assessment', :as => 'under_assessment'
 				get 'reported(.:format)', :action => 'reported', :as => 'reported'
 				get 'reported_not_adult(.:format)', :action => 'reported_not_adult', :as => 'reported_not_adult'
 				get 'requested_permanent_deletion(.:format)', :action => 'requested_permanent_deletion', :as => 'requested_permanent_deletion'
