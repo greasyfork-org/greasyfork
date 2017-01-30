27966ddabc91c6112fb935a4a860d45b71392830
Jason Barnabe <jason.barnabe@gmail.com>
2017-01-29 22:13:57 -0600

    Reconfigure thinking sphinx to use shared paths

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :rvm_ruby_version, '2.3.3'
 
 append :linked_files, 'config/database.yml', 'config/newrelic.yml', 'config/production.sphinx.conf', 'config/secrets.yml',  'config/initializers/vanilla.rb', 'config/initializers/transifex.rb', 'config/initializers/omniauth.rb', 'config/initializers/detect_language.rb', 'bin/java', 'bin/simian.jar'
 
-append :linked_dirs, 'public/forum', 'public/system', 'log', 'tmp', 'db/sphinx', 'public/data'
+append :linked_dirs, 'public/forum', 'public/system', 'log', 'tmp', 'public/data'
 
 namespace :deploy do
   after :published, "thinking_sphinx:index"
