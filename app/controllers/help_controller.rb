4cf27f2a5281043f0fb5fe858176ed27b33c730b
Jason Barnabe <jason.barnabe@gmail.com>
2017-02-15 18:54:48 -0600

    Fix install instructions from help

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -2,6 +2,7 @@ class HelpController < ApplicationController
 	before_action :authorize_for_moderators_only, :only => [:disallowed_code]
 
 	def installing_user_scripts
+		@ad_method = choose_ad_method
 		# All this content is on the home page.
 		@bots = 'noindex'
 	end
