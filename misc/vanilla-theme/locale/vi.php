7489bcb9b3f708ef360af48de71d1fe193db0e6d
Jason Barnabe <jason.barnabe@gmail.com>
2017-05-16 21:56:04 -0500

    Update locales from Transifex

--- misc/vanilla-theme/locale/vi.php
+++ misc/vanilla-theme/locale/vi.php
@@ -2,8 +2,8 @@
 $Definition['layouts.application.script_list'] = 'Scripts';
 $Definition['layouts.application.forum'] = 'Diễn đàn';
 $Definition['layouts.application.help'] = 'Hướng dẫn';
-$Definition['layouts.application.submenu'] = 'More';
-$Definition['layouts.application.advanced_search'] = 'Advanced search';
-$Definition['layouts.application.user_list'] = 'User list';
-$Definition['layouts.application.libraries'] = 'Libraries';
+$Definition['layouts.application.submenu'] = 'Thêm';
+$Definition['layouts.application.advanced_search'] = 'TÌm kiếm nâng cao';
+$Definition['layouts.application.user_list'] = 'Danh sách người dùng';
+$Definition['layouts.application.libraries'] = 'Các thư viện';
 $Definition['layouts.application.moderator_log'] = 'Moderator log';
