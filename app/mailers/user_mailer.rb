399aaa6dfa0aae2b1cdc743055f68f4825ae4b73
Jason Barnabe <jason.barnabe@gmail.com>
2017-07-25 19:39:57 -0500

    Fix user deletion email confirmation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
   def delete_confirm(user)
     mail(to: user.email, subject: 'Greasy Fork account delete confirmation') do |format|
       format.text {
-        render text: "A request was made to delete your account '#{user.name}' on Greasy Fork. To confirm this request and delete your account, go to #{user_delete_confirm_url(key: user.delete_confirmation_key)}. This must be completed in the next 24 hours or the request will be cancelled."
+        render plain: "A request was made to delete your account '#{user.name}' on Greasy Fork. To confirm this request and delete your account, go to #{user_delete_confirm_url(key: user.delete_confirmation_key)}. This must be completed in the next 24 hours or the request will be cancelled."
       }
     end
   end
