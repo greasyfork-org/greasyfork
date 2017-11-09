b0c592c233cf699b695070540a6d261314f79e8a
Jason Barnabe <jason.barnabe@gmail.com>
2017-11-08 19:49:56 -0600

    Do not rely on git being in PATH

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "https://github.com/JasonBarnabe/greasyfork.git"
 set :deploy_to, "/home/www/greasyfork"
 set :rvm_ruby_version, '2.4.2'
 
-append :linked_files, 'config/database.yml', 'config/newrelic.yml', 'config/production.sphinx.conf', 'config/secrets.yml',  'config/initializers/vanilla.rb', 'config/initializers/transifex.rb', 'config/initializers/omniauth.rb', 'config/initializers/detect_language.rb', 'bin/java', 'bin/simian.jar'
+append :linked_files, 'config/database.yml', 'config/newrelic.yml', 'config/production.sphinx.conf', 'config/secrets.yml',  'config/initializers/vanilla.rb', 'config/initializers/transifex.rb', 'config/initializers/omniauth.rb', 'config/initializers/detect_language.rb', 'bin/java', 'bin/simian.jar', 'bin/git'
 
 append :linked_dirs, 'public/forum', 'public/system', 'log', 'tmp', 'public/data'
 
--- lib/git.rb
+++ lib/git.rb
@@ -2,6 +2,7 @@ require 'open3'
 class Git
 
 	TMP_LOCATION = '/tmp/webhook'
+	GIT_PATH = Rails.root.join('bin', 'git').to_s
 
 	def self.get_contents(repo_url, file_paths_and_commits)
 		# Make a directory for this repo
@@ -9,11 +10,11 @@ class Git
 		system('mkdir', '-p', TMP_LOCATION)
 
 		begin
-			content, stderr, status = Open3.capture3('git', 'clone', '--no-checkout', repo_url, directory)
+			content, stderr, status = Open3.capture3(GIT_PATH, 'clone', '--no-checkout', repo_url, directory)
 			raise 'git clone failed' unless status.success?
 
 			file_paths_and_commits.each do |file_path, commit|
-				content, stderr, status = Open3.capture3('git', 'show', "#{commit}:#{file_path}", chdir: directory)
+				content, stderr, status = Open3.capture3(GIT_PATH, 'show', "#{commit}:#{file_path}", chdir: directory)
 				raise stderr unless status.success?
 				yield [file_path, commit, content]
 			end
