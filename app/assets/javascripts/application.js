ade8fbbfb71fa3e57e6929936b5b7bb1916c51fa
Jason Barnabe <jason.barnabe@gmail.com>
2017-12-01 17:52:24 -0600

    Remove jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require rails-ujs
 //= require jsonlylightbox
 //= require lightbox
 //= require everything
