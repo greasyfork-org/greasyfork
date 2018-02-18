ef4d4e013422c19d32558acdd7462d3b3ad31f64
Jason Barnabe <jason.barnabe@gmail.com>
2018-02-17 19:40:50 -0600

    Update locales from Transifex

--- misc/vanilla-theme/locale/de.php
+++ misc/vanilla-theme/locale/de.php
@@ -4,6 +4,6 @@ $Definition['layouts.application.forum'] = 'Forum';
 $Definition['layouts.application.help'] = 'Hilfe';
 $Definition['layouts.application.submenu'] = 'mehr';
 $Definition['layouts.application.advanced_search'] = 'Erweiterte Suche';
-$Definition['layouts.application.user_list'] = 'Benutzer Liste';
+$Definition['layouts.application.user_list'] = 'Benutzerliste';
 $Definition['layouts.application.libraries'] = 'Bibliotheken';
 $Definition['layouts.application.moderator_log'] = 'Moderator Protokoll';
