b48136991820b953cf6e7f249cfa98b7e7ff92d5
Jason Barnabe <jason.barnabe@gmail.com>
2018-04-02 21:24:29 -0500

    Try recaptcha.net for a recaptcha that works in China

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,5 @@
 Recaptcha.configure do |config|
   config.site_key  = Rails.application.secrets.recaptcha_site_key
   config.secret_key = Rails.application.secrets.recaptcha_secret_key
+  config.api_server_url = 'https://www.recaptcha.net/recaptcha/api.js'
 end
