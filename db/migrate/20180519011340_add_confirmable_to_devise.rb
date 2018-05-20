19eb93828e31e586ff56d9a23f84b0d4844248f6
Jason Barnabe <jason.barnabe@gmail.com>
2018-05-20 10:23:33 -0500

    Add limit to confirmation token column so the index can be applied

--- db/migrate/20180519011340_add_confirmable_to_devise.rb
+++ db/migrate/20180519011340_add_confirmable_to_devise.rb
@@ -1,7 +1,7 @@
 class AddConfirmableToDevise < ActiveRecord::Migration[5.1]
   # Note: You can't use change, as User.update_all will fail in the down migration
   def up
-    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmation_token, :string, limit: 50
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
     # add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
