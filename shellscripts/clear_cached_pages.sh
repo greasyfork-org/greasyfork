fd701f2bc1f3804f22211e1bccf588c9a92d29f7
Jason Barnabe <jason.barnabe@gmail.com>
2018-07-15 15:44:03 -0500

    Ignore stderr when clearing cache files

--- shellscripts/clear_cached_pages.sh
+++ shellscripts/clear_cached_pages.sh
@@ -1,2 +1,2 @@
-find /www/greasyfork/shared/tmp/cached_pages/ -mmin +60 -type f -delete
-find /www/greasyfork/shared/tmp/cached_pages/ -mindepth 1 -type d -empty -delete
+find /www/greasyfork/shared/tmp/cached_pages/ -mmin +60 -type f -delete 2>/dev/null
+find /www/greasyfork/shared/tmp/cached_pages/ -mindepth 1 -type d -empty -delete 2>/dev/null
