6e32a54acae677d9e0a340f2ba0eb5d8bf8927ee
Jason Barnabe <jason.barnabe@gmail.com>
2018-09-23 19:32:48 -0500

    Show pattern of allowed requires #556

---
+++ test/application_system_test_case.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+end
+
+Capybara.server = :webrick
