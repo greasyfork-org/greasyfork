2b9d6fa32eb4efdded8d129bcaae626557cf5895
Jason Barnabe <jason.barnabe@gmail.com>
2018-09-23 20:05:26 -0500

    Fix chrome crash

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
 end
 
 Capybara.server = :webrick
