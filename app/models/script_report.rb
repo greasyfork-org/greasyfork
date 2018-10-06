626af53f1b61b25ee5d59eee0e8b30587006eafa
Jason Barnabe <jason.barnabe@gmail.com>
2018-10-05 21:01:20 -0500

    Show counts of old reports in header to mods

--- app/models/script_report.rb
+++ app/models/script_report.rb
@@ -1,6 +1,9 @@
 class ScriptReport < ApplicationRecord
   belongs_to :script
   belongs_to :reference_script, class_name: 'Script'
+
+  scope :unresolved, -> { where(resolved: false) }
+  scope :unresolved_old, -> { unresolved.where(['created_at < ?', 3.days.ago]) }
   
   validates :copy_details, :additional_info, presence: true
 
