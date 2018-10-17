a15f9fd2dca6d0712cde6969c9c1b1c54cde354c
Jason Barnabe <jason.barnabe@gmail.com>
2018-10-16 19:25:14 -0500

    Update locales from  Transifex

--- misc/vanilla-theme/locale/ko.php
+++ misc/vanilla-theme/locale/ko.php
@@ -6,4 +6,4 @@ $Definition['layouts.application.submenu'] = '더 보기...';
 $Definition['layouts.application.advanced_search'] = '상세 검색';
 $Definition['layouts.application.user_list'] = '유저 목록';
 $Definition['layouts.application.libraries'] = '라이브러리';
-$Definition['layouts.application.moderator_log'] = '관리자 기록';
+$Definition['layouts.application.moderator_log'] = '관리자 로그';
