d693f5ee0f1793a5cfac86213b3a2279ba575a12
Jason Barnabe <jason.barnabe@gmail.com>
2018-10-26 19:08:45 -0500

    Do not report on script report where the script has bene deleted

--- app/models/script_report.rb
+++ app/models/script_report.rb
@@ -2,8 +2,8 @@ class ScriptReport < ApplicationRecord
   belongs_to :script
   belongs_to :reference_script, class_name: 'Script'
 
-  scope :unresolved, -> { where(resolved: false) }
-  scope :unresolved_old, -> { unresolved.where(['created_at < ?', 3.days.ago]) }
+  scope :unresolved, -> { where(resolved: false).joins(:script) }
+  scope :unresolved_old, -> { unresolved.where(['script_reports.created_at < ?', 3.days.ago]) }
   
   validates :copy_details, :additional_info, presence: true
 
