6e41032fce86ef99b325ea2925cab18f50a2e525
Jason Barnabe <jason.barnabe@gmail.com>
2018-12-29 18:31:55 -0600

    Fix error raised by sidebar code when no sidebar present

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector(".close-sidebar").addEventListener("click", function() {
-    document.querySelector(".sidebar").classList.add("collapsed");
-    document.querySelector(".open-sidebar").classList.add("sidebar-collapsed");
-  });
-  document.querySelector(".open-sidebar").addEventListener("click", function() {
-    document.querySelector(".sidebar").classList.remove("collapsed");
-    document.querySelector(".open-sidebar").classList.remove("sidebar-collapsed");
-  });
+  if (document.querySelector(".close-sidebar")) {
+    document.querySelector(".close-sidebar").addEventListener("click", function() {
+      document.querySelector(".sidebar").classList.add("collapsed");
+      document.querySelector(".open-sidebar").classList.add("sidebar-collapsed");
+    });
+  }
+  if (document.querySelector(".open-sidebar")) {
+    document.querySelector(".open-sidebar").addEventListener("click", function() {
+      document.querySelector(".sidebar").classList.remove("collapsed");
+      document.querySelector(".open-sidebar").classList.remove("sidebar-collapsed");
+    });
+  }
 });
