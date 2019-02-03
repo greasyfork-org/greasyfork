610553f235588a5ab012748d6f8c3c81e0b98fa9
Jason Barnabe <jason.barnabe@gmail.com>
2019-02-03 13:08:29 -0600

    Fix import screen for multi-author scripts #311

---
+++ test/system/users/import_test.rb
@@ -0,0 +1,8 @@
+require "application_system_test_case"
+
+class ImportTest < ApplicationSystemTestCase
+  test "import page" do
+    login_as(User.first, scope: :user)
+    visit import_start_url
+  end
+end
