2774cb5c065bee67895638aa681fb6f83e39fefd
Jason Barnabe <jason.barnabe@gmail.com>
2019-03-19 18:49:52 -0500

    Fix script report notification emails

--- app/mailers/script_report_mailer.rb
+++ app/mailers/script_report_mailer.rb
@@ -1,15 +1,15 @@
 class ScriptReportMailer < ApplicationMailer
 
   def report_created(report, site_name)
-    mail_to_offender(report, site_name, "Your script #{report.script.default_name} on #{site_name} has been reported as being an unauthorized copy. You can review this report at #{script_script_report_url(report.script, report)} and submit a rebuttal. If you do not submit a rebuttal, your script may be deleted by moderators. If you have any questions, please visit https://greasyfork.org/forum/.")
+    mail_to_offender(report, site_name, "Your script #{report.script.default_name} on #{site_name} has been reported as being an unauthorized copy. You can review this report at #{script_script_report_url(report.script, report, locale: nil)} and submit a rebuttal. If you do not submit a rebuttal, your script may be deleted by moderators. If you have any questions, please visit https://greasyfork.org/forum/.")
   end
 
   def report_rebutted(report, site_name)
-    mail_to_reporter(report, site_name, "Your report on script #{report.script.default_name} on #{site_name} has received a reply. You can review this reply at #{script_script_report_url(report.script, report)}. A moderator will review your report and this reply and decide on the result. If you have any questions, please visit https://greasyfork.org/forum/.")
+    mail_to_reporter(report, site_name, "Your report on script #{report.script.default_name} on #{site_name} has received a reply. You can review this reply at #{script_script_report_url(report.script, report, locale: nil)}. A moderator will review your report and this reply and decide on the result. If you have any questions, please visit https://greasyfork.org/forum/.")
   end
 
   def report_upheld_offender(report, site_name)
-    mail_to_offender(report, site_name, "Your script #{report.script.default_name} on #{site_name} has been deleted by a moderator due to a report of it being unauthorized code. You can review this report at #{script_script_report_url(report.script, report)}. If you have any questions, please visit https://greasyfork.org/forum/.")
+    mail_to_offender(report, site_name, "Your script #{report.script.default_name} on #{site_name} has been deleted by a moderator due to a report of it being unauthorized code. You can review this report at #{script_script_report_url(report.script, report, locale: nil)}. If you have any questions, please visit https://greasyfork.org/forum/.")
   end
 
   def report_upheld_reporter(report, author_deleted, site_name)
@@ -17,7 +17,7 @@ class ScriptReportMailer < ApplicationMailer
   end
 
   def report_dismissed_offender(report, site_name)
-    mail_to_offender(report, site_name, "A report on your script #{report.script.default_name} on #{site_name} has been dismissed by a moderator. Your script will remain active. You can review this report at #{script_script_report_url(report.script, report)}. If you have any questions, please visit https://greasyfork.org/forum/.")
+    mail_to_offender(report, site_name, "A report on your script #{report.script.default_name} on #{site_name} has been dismissed by a moderator. Your script will remain active. You can review this report at #{script_script_report_url(report.script, report, locale: nil)}. If you have any questions, please visit https://greasyfork.org/forum/.")
   end
 
   def report_dismissed_reporter(report, site_name)
