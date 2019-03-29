211613ad2170202a5c4e1402992190f86a547030
Jason Barnabe <jason.barnabe@gmail.com>
2019-03-29 13:27:05 -0500

    Fix script list json #311

--- app/controllers/concerns/script_listings.rb
+++ app/controllers/concerns/script_listings.rb
@@ -315,10 +315,10 @@ module ScriptListings
         render :index
       }
       format.json {
-        render json: params[:meta] == '1' ? {count: @scripts.count} : @scripts.as_json(include: :user)
+        render json: params[:meta] == '1' ? {count: @scripts.count} : @scripts.as_json(include: :users)
       }
       format.jsonp {
-        render json: params[:meta] == '1' ? {count: @scripts.count} : @scripts.as_json(include: :user), callback: clean_json_callback_param
+        render json: params[:meta] == '1' ? {count: @scripts.count} : @scripts.as_json(include: :users), callback: clean_json_callback_param
       }
     end
   end
--- test/controllers/scripts_controller_test.rb
+++ test/controllers/scripts_controller_test.rb
@@ -12,4 +12,10 @@ class ScriptsControllerTest < ActionDispatch::IntegrationTest
     get script_url(Script.first, locale: :en, format: :json)
     assert_response 200
   end
+
+  test 'render redistributable as json' do
+    Script.update_all(approve_redistribution: true)
+    get redistributable_scripts_url(locale: :en, format: :json)
+    assert_response 200
+  end
 end
