68ab324c6fd83f65b384f127bbd8f1176346522e
Jason Barnabe <jason.barnabe@gmail.com>
2019-07-29 21:53:24 -0500

    Update recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,4 @@
 Recaptcha.configure do |config|
   config.site_key  = Rails.application.secrets.recaptcha_site_key
   config.secret_key = Rails.application.secrets.recaptcha_secret_key
-  config.api_server_url = 'https://www.recaptcha.net/recaptcha/api.js'
 end
