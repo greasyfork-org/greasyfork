411b9f2989e19d543c372b3402273176be007e75
Jason Barnabe <jason.barnabe@gmail.com>
2019-08-12 18:08:59 -0500

    Update locales from Transifex

--- misc/vanilla-plugin/locale/he.php
+++ misc/vanilla-plugin/locale/he.php
@@ -2,42 +2,42 @@
 /* Label for inputting a script ID when creating/editing a discussion */
 $Definition['Item ID:'] = 'מזהה סקריפט';
 // Appended to "Discussions" when viewing a list of discussions on scripts by a user. %s is the user name.
-$Definition['on scripts by %s'] = '%s צופה בסקריפט';
+$Definition['on scripts by %s'] = 'על סקריפטים של %s';
 /*
  *  Label for the radio buttons for rating a script
  **/
 $Definition['RatingLabel'] = 'דרג את הסקריפט';
 # Label for the radio buttons for updating a rating on a script
-$Definition['RatingUpdateLabel'] = 'עדכן את הדירוג';
+$Definition['RatingUpdateLabel'] = 'עדכן את הדירוג שלך:';
 # Label for the radio button for not rating a script
-$Definition['RatingNone'] = 'ללא דירוג- רק שאלה, הערה, בקשה להוספת פיצ\'ר או דיווח על תקלה';
+$Definition['RatingNone'] = 'ללא דירוג- רק שאלה, הערה, בקשה להוספת יכולת או דיווח על תקלה';
 ## Label for the radio button for reporting a script
 $Definition['RatingReport'] = '<div class="report"> דווח על הסקריפט למנהלים להסרתו, לדוגמא: <ul><li> תוכנה זדונית - לתאר איזה חלק בקוד הוא זדוני </li><li> רישוי / זכויות יוצרים - תאר ותן קישור למקור המקורי של הקוד ותספק ראיות אחרות </li></ul></div>';
 /** Label for the radio button for rating a script "Bad" */
 $Definition['RatingBad'] = 'לא טוב- הסקריפט אינו עובד';
 /** Label for the radio button for rating a script "OK" **/
-$Definition['RatingOK'] = 'בסדר- הסקריפט עובד, אך סובל מבעיות מסויימות';
+$Definition['RatingOK'] = 'בסדר- הסקריפט עובד, אך סובל מבעיות משמעותיות';
 // Label for the radio button for rating a script "Good"
-$Definition['RatingGood'] = 'מעולה- הסקירפט עובד באופן תקין';
+$Definition['RatingGood'] = 'מעולה- הסקירפט עובד טוב';
 // Category name for Site Feedback
 $Definition['Categories.general.Name'] = 'ביקורת אתר';
 // Category description for Site Feedback
-$Definition['Categories.general.Description'] = 'דיווח על באג, בקשת מאפיין ודברים אחרים לגבי גריזי פורק';
+$Definition['Categories.general.Description'] = 'דיווחים על באגים, בקשת יכולות, ודברים אחרים לגבי Greasy Fork עצמו.';
 // Category name for Development
 $Definition['Categories.script-development.Name'] = 'פיתוח';
 // Category description for Development
-$Definition['Categories.script-development.Description'] = 'דיונים טכניים על פיתוח התסריט.';
+$Definition['Categories.script-development.Description'] = 'דיונים טכניים על פיתוח סקריפטי משתמש.';
 // Category name for Script Creation Requests
 $Definition['Categories.script-requests.Name'] = 'בקשות ליצירת סקריפט';
 // Category description for Script Creation Requests
-$Definition['Categories.script-requests.Description'] = 'בקש יצירת סקריפט עבורך';
+$Definition['Categories.script-requests.Description'] = 'בקש ממישהו שכתוב עבורך סקריפט.';
 // Category name for Script Discussions
-$Definition['Categories.script-discussions.Name'] = 'דיונים על הסקריפט';
+$Definition['Categories.script-discussions.Name'] = 'דיוני סקריפטים';
 // Category description for Script Discussions
-$Definition['Categories.script-discussions.Description'] = 'דיונים על סקריפטים שנשלחו לגריזי פורק';
+$Definition['Categories.script-discussions.Description'] = 'דיונים על סקריפטים שנשלחו ל-Greasy Fork';
 // Link on Recent Discussions to hide script discussions
 $Definition['Turn on filter'] = 'הסתר דיוני סקריפט';
 // Link on Recent Discussions to show script discussions
 $Definition['Turn off filter'] = 'הצג דיוני סקריפט';
 // Link on forum profile pointing to main site profile
-$Definition['Greasy Fork Profile'] = 'פרופיל greesy fork';
+$Definition['Greasy Fork Profile'] = 'פרופיל Greesy Fork';
--- misc/vanilla-theme/locale/he.php
+++ misc/vanilla-theme/locale/he.php
@@ -4,6 +4,6 @@ $Definition['layouts.application.forum'] = 'פורום';
 $Definition['layouts.application.help'] = 'עזרה';
 $Definition['layouts.application.submenu'] = 'עוד';
 $Definition['layouts.application.advanced_search'] = 'חיפוש מתקדם';
-$Definition['layouts.application.user_list'] = 'רשימת משתמש';
+$Definition['layouts.application.user_list'] = 'רשימת משתמשים';
 $Definition['layouts.application.libraries'] = 'ספריות';
 $Definition['layouts.application.moderator_log'] = 'יומן מנהל';
