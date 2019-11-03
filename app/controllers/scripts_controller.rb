62ab832b2b0c55aa140ff9475d085ea92d013d52
Jason Barnabe <jason.barnabe@gmail.com>
2019-11-02 20:32:58 -0500

    Undo a change to caching code

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -821,7 +821,7 @@ private
         FileUtils.mkdir_p(cache_path.parent)
         File.write(cache_path, response_body)
         # nginx does not seem to automatically compress with try_files, so give it a .gz to use.
-        system("gzip", "-f", cache_path.to_s)
+        system("gzip", cache_path.to_s)
       end
     end
   end
