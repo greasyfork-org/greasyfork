2611cd376a8a1604da54acd1026a5ca17accf01d
Jason Barnabe <jason.barnabe@gmail.com>
2019-12-07 19:45:31 -0600

    Update sprockets

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,5 @@
+//= link_tree ../images
+//= link_tree ../../../vendor/assets
+//= link application.css
+//= link application.js
+//= link ace.js
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,11 +7,3 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 # Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
-Rails.application.config.assets.precompile << Proc.new { |path,fn| fn.starts_with?(Rails.root.join('vendor').to_s)}
-Rails.application.config.assets.precompile << 'ace.js'
