2551e261b0afa800b9e2a90b6ba49eed0f336d21
Jason Barnabe <jason.barnabe@gmail.com>
2019-12-25 11:43:18 -0600

    Use Rails 5.2 defaults

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :banned?
 
