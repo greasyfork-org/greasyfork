dba3905eeb14372014fb4f3dedbb84d0b23b571d
Jason Barnabe <jason.barnabe@gmail.com>
2019-12-26 16:23:05 -0600

    Revert "Revert "Upgrade to Rails 6.0.2.1""
    This reverts commit 3eb04885262cff52cc6d5c4459c5e826c40f8671.

--- config/application.rb
+++ config/application.rb
@@ -12,22 +12,15 @@ module Greasyfork
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Overridden in config/initializers/omniauth.rb
     config.available_auths = {}
 
     config.active_job.queue_adapter = :delayed_job
-    
+
     config.active_record.observers = :script_observer
   end
 end
