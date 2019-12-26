bd5a8dfb369f8f49d4e69b14807232c6be5a0ce4
Jason Barnabe <jason.barnabe@gmail.com>
2019-12-26 15:43:10 -0600

    Move i18n settings to initializer

---
+++ config/initializers/i18n.rb
@@ -0,0 +1,6 @@
+I18n.config.enforce_available_locales = true
+# locales good enough to link to
+# names from https://api.drupal.org/api/drupal/includes!iso.inc/function/_locale_get_predefined_list/7
+Rails.application.config.available_locales = {'ar' => 'العَرَبِيةُ', 'bg' => 'Български', 'cs' => 'Čeština', 'da' => 'Dansk', 'de' => 'Deutsch', 'el' => 'Ελληνικά', 'en' => 'English', 'eo' => 'Esperanto', 'es' => 'Español', 'fi' => 'Suomi', 'fr' => 'Français', 'fr-CA' => 'Français canadien', 'he' => 'עברית', 'hu' => 'Magyar', 'id' => 'Bahasa Indonesia', 'it' => 'Italiano', 'ja' => '日本語', 'ko' => '한국어', 'nb' => 'Bokmål',  'nl' => 'Nederlands', 'pl' => 'Polski', 'pt-BR' => 'Português do Brasil', 'ro' => 'Română', 'ru' => 'Русский', 'sk' => 'Slovenčina', 'sr' => 'srpski', 'sv' => 'Svenska', 'th' => 'ภาษาไทย', 'tr' => 'Türkçe', 'uk' => 'Українська', 'vi' => 'Tiếng Việt', 'zh-CN' => '简体中文', 'zh-TW' => '繁體中文'}
+Rails.application.config.help_translate_url = 'https://github.com/JasonBarnabe/greasyfork/wiki/Translating-Greasy-Fork'
+Rails.application.config.i18n.fallbacks = [:en]
