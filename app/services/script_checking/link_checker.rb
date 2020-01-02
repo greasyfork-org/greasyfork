14a85fc6b57c466fe88c04ea16f62a4475d7a486
Jason Barnabe <jason.barnabe@gmail.com>
2020-01-01 21:16:22 -0600

    URL check should also check additional info

--- app/services/script_checking/link_checker.rb
+++ app/services/script_checking/link_checker.rb
@@ -9,16 +9,19 @@ class ScriptChecking::LinkChecker
   class << self
     def check(script_version)
       blocked_urls = BlockedScriptUrl.all
-      code = script_version.code
 
-      blocked_urls.each do |bu|
-        return ScriptChecking::Result.new(ScriptChecking::Result::RESULT_CODE_BAN, bu.public_reason, bu.private_reason, bu) if code.include?(bu.url)
-      end
+      things_to_check = ([script_version.code] + script_version.active_localized_attributes.select { |aa| aa.attribute_key == 'additional_info'}.map(&:attribute_value)).compact
 
-      redirect_destinations = code.scan(Regexp.union(*REDIRECT_PATTERNS)).uniq.map { |url| resolve(url) }.compact.uniq
+      things_to_check.each do |thing_to_check|
+        blocked_urls.each do |bu|
+          return ScriptChecking::Result.new(ScriptChecking::Result::RESULT_CODE_BAN, bu.public_reason, bu.private_reason, bu) if thing_to_check.include?(bu.url)
+        end
 
-      blocked_urls.each do |bu|
-        return ScriptChecking::Result.new(ScriptChecking::Result::RESULT_CODE_BAN, bu.public_reason, bu.private_reason, bu) if redirect_destinations.include?(bu.url)
+        redirect_destinations = thing_to_check.scan(Regexp.union(*REDIRECT_PATTERNS)).uniq.map { |url| resolve(url) }.compact.uniq
+
+        blocked_urls.each do |bu|
+          return ScriptChecking::Result.new(ScriptChecking::Result::RESULT_CODE_BAN, bu.public_reason, bu.private_reason, bu) if redirect_destinations.include?(bu.url)
+        end
       end
 
       return ScriptChecking::Result.new(ScriptChecking::Result::RESULT_CODE_OK)
--- test/services/script_checking/link_checker_test.rb
+++ test/services/script_checking/link_checker_test.rb
@@ -3,23 +3,37 @@ require 'test_helper'
 class ScriptChecking::LinkCheckerTest < ActiveSupport::TestCase
 
   test 'not blocked' do
-    assert_equal ScriptChecking::Result::RESULT_CODE_OK, check_script('https://example.com/this-is-ok').code
+    assert_equal ScriptChecking::Result::RESULT_CODE_OK, check_script_with_url_in_code('https://example.com/this-is-ok').code
   end
 
   test 'directly blocked' do
-    assert_equal ScriptChecking::Result::RESULT_CODE_BAN, check_script('https://example.com/unique-test-value').code
+    assert_equal ScriptChecking::Result::RESULT_CODE_BAN, check_script_with_url_in_code('https://example.com/unique-test-value').code
   end
 
   test 'bit.ly not blocked' do
-    assert_equal ScriptChecking::Result::RESULT_CODE_OK, check_script('https://bit.ly/2uaUwLP').code
+    assert_equal ScriptChecking::Result::RESULT_CODE_OK, check_script_with_url_in_code('https://bit.ly/2uaUwLP').code
   end
 
   test 'bit.ly blocked' do
-    assert_equal ScriptChecking::Result::RESULT_CODE_BAN, check_script('https://bit.ly/2SKn88z').code
+    assert_equal ScriptChecking::Result::RESULT_CODE_BAN, check_script_with_url_in_code('https://bit.ly/2SKn88z').code
   end
 
-  def check_script(url)
+  test 'use in additional info not blocked' do
+    assert_equal ScriptChecking::Result::RESULT_CODE_OK, check_script_with_url_in_additional_info('https://example.com/this-is-ok').code
+  end
+
+  test 'use in additional info blocked' do
+    assert_equal ScriptChecking::Result::RESULT_CODE_BAN, check_script_with_url_in_additional_info('https://example.com/unique-test-value').code
+  end
+
+  def check_script_with_url_in_code(url)
     script_version = ScriptVersion.new(code: "location.href = '" + url + "'")
+    ScriptChecking::LinkChecker.check(script_version)
+  end
+
+  def check_script_with_url_in_additional_info(url)
+    script_version = ScriptVersion.new
+    script_version.localized_attributes.build(attribute_key: 'additional_info', locale: locales(:english), attribute_value: url)
     ScriptChecking::LinkChecker.check(script_version)
   end
 end
