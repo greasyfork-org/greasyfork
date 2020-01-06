c329d77e32b947e91e1ed3b2b19f465a2ec532e2
Jason Barnabe <jason.barnabe@gmail.com>
2020-01-05 20:34:06 -0600

    Avoid doing all syncs at the same time

--- runnerscripts/auto_sync_scripts.rb
+++ runnerscripts/auto_sync_scripts.rb
@@ -2,7 +2,7 @@ require 'script_importer/script_syncer'
 
 puts "Started at #{DateTime.now}."
 
-Script.where('script_sync_type_id = 2').where('last_attempted_sync_date < DATE_SUB(UTC_TIMESTAMP(), INTERVAL 1 DAY)').find_each(batch_size: 10) do |script|
+Script.where('script_sync_type_id = 2').where('last_attempted_sync_date < DATE_SUB(UTC_TIMESTAMP(), INTERVAL 1 DAY)').order(:last_attempted_sync_date).limit(100).find_each(batch_size: 10) do |script|
 	begin
 		result = ScriptImporter::ScriptSyncer.sync(script)
 		puts "#{script.id} - #{result} - #{script.sync_error}"
