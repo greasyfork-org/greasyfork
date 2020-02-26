98b1f17f4e40cbd8d3d45fe37b3ad42d4298ea8c
Jason Barnabe <jason.barnabe@gmail.com>
2020-02-26 15:51:22 -0600

    Add more ordering on similarities

--- app/controllers/admin/script_similarities_controller.rb
+++ app/controllers/admin/script_similarities_controller.rb
@@ -4,7 +4,7 @@ module Admin
       @top_similarities = ScriptSimilarity
                               .joins(:script, :other_script)
                               .where(scripts: { script_delete_type_id: nil }, other_scripts_script_similarities: { script_delete_type_id: nil })
-                              .order(similarity: :desc)
+                              .order(similarity: :desc, script_id: :asc, other_script_id: :asc)
                               .limit(100)
       @run_count = ScriptSimilarity.select(:script_id).distinct.count
       @total_count = Script.not_deleted.count
