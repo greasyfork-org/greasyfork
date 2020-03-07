dcb6825c0344576eec4bc58737c0c5237a1a987b
Jason Barnabe <jason.barnabe@gmail.com>
2020-03-07 17:07:29 -0600

    Enqueue duplicate enqueuer on startup

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://192.168.149.153:6379/0' }
 
-  #config.on(:startup) do
-  #  ScriptDuplicateCheckerQueueingJob.perform_later if Sidekiq::Queue.new('background').none? {|sq| sq.item['class'] == 'ScriptDuplicateCheckerQueueingJob'}
-  #end
+  config.on(:startup) do
+    ScriptDuplicateCheckerQueueingJob.perform_later if Sidekiq::Queue.new('background').none? { |sq| sq.item['wrapped'] == 'ScriptDuplicateCheckerQueueingJob' }
+  end
 end if Rails.env.production?
