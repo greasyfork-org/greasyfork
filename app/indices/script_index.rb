733caf8fd7b18d64631fe11b34d59c7b205e6823
Jason Barnabe <jason.barnabe@gmail.com>
2020-03-07 10:05:42 -0600

    Temporarily combine all script fields in search to deal with slow indexing queries

--- app/indices/script_index.rb
+++ app/indices/script_index.rb
@@ -1,9 +1,10 @@
 ThinkingSphinx::Index.define :script, :with => :active_record, :delta => ThinkingSphinx::Deltas::DelayedDelta do
 
   # fields
-  indexes localized_names.attribute_value, :as => 'name'
-  indexes localized_descriptions.attribute_value, :as => 'description'
-  indexes localized_additional_infos.attribute_value, :as => 'additional_info'
+  #indexes localized_names.attribute_value, :as => 'name'
+  #indexes localized_descriptions.attribute_value, :as => 'description'
+  #indexes localized_additional_infos.attribute_value, :as => 'additional_info'
+  indexes localized_attributes.attribute_value, as: 'combined_text'
 
   indexes users.name, :as => :author
 
@@ -19,10 +20,11 @@ ThinkingSphinx::Index.define :script, :with => :active_record, :delta => Thinkin
   where 'script_type_id IN (1,3) and script_delete_type_id is null and review_state != "required"'
 
   set_property :field_weights => {
-    :name => 10,
-    :author => 5,
-    :description => 2,
-    :additional_info => 1
+    #name: 10,
+    combined_text: 10,
+    author: 5,
+    #description: 2,
+    #additional_info: 1,
   }
 
 end
