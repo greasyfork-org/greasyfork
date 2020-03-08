56637181f4d028eded555644ac192bbba413c550
Jason Barnabe <jason.barnabe@gmail.com>
2020-03-08 15:11:38 -0500

    Don't enqueue a duplicate checker if it's already enqueued

--- app/jobs/script_duplicate_checker_queueing_job.rb
+++ app/jobs/script_duplicate_checker_queueing_job.rb
@@ -3,6 +3,9 @@ class ScriptDuplicateCheckerQueueingJob < ApplicationJob
   self.queue_adapter = :sidekiq if Rails.env.production?
 
   def perform
+    currently_running = Sidekiq::Queue.new('low')
+                            .select { |sq| sq.item['wrapped'] == 'ScriptDuplicateCheckerJob' }
+                            .map{|job| job.args.first['arguments'].first }
     Script
         .not_deleted
         .left_joins(:script_similarities)
@@ -10,6 +13,7 @@ class ScriptDuplicateCheckerQueueingJob < ApplicationJob
         .order('min(script_similarities.checked_at)', :id)
         .limit(5)
         .pluck('scripts.id')
+        .reject { |id| currently_running.include?(id) }
         .each { |id| ScriptDuplicateCheckerJob.perform_later(id) }
     self.class.perform_later
   end
