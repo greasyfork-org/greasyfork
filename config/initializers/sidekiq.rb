07ce17e4b8878ba06143d2c2fb77433741157a6f
Jason Barnabe <jason.barnabe@gmail.com>
2020-03-09 15:57:07 -0500

    Startup job needs to be configured on server maybe

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,8 @@
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://192.168.149.153:6379/0' }
+end if Rails.env.production?
 
+Sidekiq.configure_server do |config|
   config.on(:startup) do
     BackgroundJob.perform_later if Sidekiq::Queue.new('background').none? { |sq| sq.item['wrapped'] == 'BackgroundJob' } &&
         Sidekiq::Workers.new.none? {|process_id, thread_id, work| work['payload']['wrapped'] == 'BackgroundJob' }
