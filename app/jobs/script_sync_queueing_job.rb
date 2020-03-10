2dd08da46ee799125498f52b84f3445b297d86f5
Jason Barnabe <jason.barnabe@gmail.com>
2020-03-09 22:06:11 -0500

    Fix some SQL

--- app/jobs/script_sync_queueing_job.rb
+++ app/jobs/script_sync_queueing_job.rb
@@ -5,7 +5,7 @@ class ScriptSyncQueueingJob < ApplicationJob
   def perform
     Script
         .where(script_sync_type_id: 2)
-        .where('last_attempted_sync_date < DATE_SUB(UTC_TIMESTAMP(), INTERVAL 12 HOURS)')
+        .where('last_attempted_sync_date < DATE_SUB(UTC_TIMESTAMP(), INTERVAL 12 HOUR)')
         .order(:last_attempted_sync_date)
         .limit(1000)
         .each do |script|
