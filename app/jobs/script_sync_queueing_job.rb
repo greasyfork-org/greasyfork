0e05857659d39b3f6631ded15ad8a91e8fc2a064
Jason Barnabe <jason.barnabe@gmail.com>
2020-03-09 21:53:33 -0500

    Reduce URL syncing period to 12 hours, ensure runs are spread out

--- app/jobs/script_sync_queueing_job.rb
+++ app/jobs/script_sync_queueing_job.rb
@@ -5,9 +5,10 @@ class ScriptSyncQueueingJob < ApplicationJob
   def perform
     Script
         .where(script_sync_type_id: 2)
-        .where('last_attempted_sync_date < DATE_SUB(UTC_TIMESTAMP(), INTERVAL 1 DAY)')
+        .where('last_attempted_sync_date < DATE_SUB(UTC_TIMESTAMP(), INTERVAL 12 HOURS)')
         .order(:last_attempted_sync_date)
-        .find_each do |script|
+        .limit(1000)
+        .each do |script|
       ScriptSyncJob.perform_later(script.id)
     end
   end
