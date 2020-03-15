a42cb44c8295b28bf12225047e373df72e9b7d8b
Jason Barnabe <jason.barnabe@gmail.com>
2020-03-15 18:52:09 -0500

    Disable delete job due to Sphinx stufF

--- app/jobs/script_delete_job.rb
+++ app/jobs/script_delete_job.rb
@@ -3,7 +3,8 @@ class ScriptDeleteJob < ApplicationJob
   self.queue_adapter = :sidekiq if Rails.env.production?
 
   def perform
-    Script.where(locked: true).where.not(script_delete_type_id: nil).where(['deleted_at < ?', 1.year.ago]).limit(100).each(&:destroy)
+    # Temp disabled - https://github.com/pat/thinking-sphinx/issues/1161
+    # Script.where(locked: true).where.not(script_delete_type_id: nil).where(['deleted_at < ?', 1.year.ago]).limit(100).each(&:destroy)
     self.class.set(wait: 1.hour).perform_later
   end
 end
