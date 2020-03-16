e932203cd7cd3d5694dbca395b6373e8ba9394c6
Jason Barnabe <jason.barnabe@gmail.com>
2020-03-15 20:51:04 -0500

    Re-enable old deleted script delete

--- app/jobs/script_delete_job.rb
+++ app/jobs/script_delete_job.rb
@@ -3,8 +3,10 @@ class ScriptDeleteJob < ApplicationJob
   self.queue_adapter = :sidekiq if Rails.env.production?
 
   def perform
-    # Temp disabled - https://github.com/pat/thinking-sphinx/issues/1161
-    # Script.where(locked: true).where.not(script_delete_type_id: nil).where(['deleted_at < ?', 1.year.ago]).limit(100).each(&:destroy)
+    # Delete callbacks don't work on a remote Sphinx. We don't need that anyway, as these already don't show in search results.
+    ThinkingSphinx::Callbacks.suspend do
+      Script.where(locked: true).where.not(script_delete_type_id: nil).where(['deleted_at < ?', 1.year.ago]).limit(100).each(&:destroy)
+    end
     self.class.set(wait: 1.hour).perform_later
   end
 end
