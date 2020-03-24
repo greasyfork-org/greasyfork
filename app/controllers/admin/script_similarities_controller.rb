72be47ab473b49d063a8e72f589d59909a75655c
Jason Barnabe <jason.barnabe@gmail.com>
2020-03-23 20:56:55 -0500

    Don't do includes on script similarities - actually it's slow

--- app/controllers/admin/script_similarities_controller.rb
+++ app/controllers/admin/script_similarities_controller.rb
@@ -2,10 +2,13 @@ module Admin
   class ScriptSimilaritiesController < BaseController
     def index
       @top_similarities = ScriptSimilarity
-                          .includes(script: :localized_names, other_script: :localized_names)
+                          .joins(:script, :other_script)
                           .where(scripts: { script_delete_type_id: nil }, other_scripts_script_similarities: { script_delete_type_id: nil })
                           .order(similarity: :desc, script_id: :asc, other_script_id: :asc)
                           .limit(100)
+      # This is slow
+      # .includes(script: :localized_names, other_script: :localized_names)
+
       @run_count = ScriptSimilarity.select(:script_id).distinct.count
       @total_count = Script.not_deleted.count
     end
