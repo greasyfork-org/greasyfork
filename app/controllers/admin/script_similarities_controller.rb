0333a62526af116a474ff5985d32df1d5b582e9c
Jason Barnabe <jason.barnabe@gmail.com>
2020-04-12 16:59:52 -0500

    Add pagination to admin similarities

--- app/controllers/admin/script_similarities_controller.rb
+++ app/controllers/admin/script_similarities_controller.rb
@@ -1,11 +1,15 @@
 module Admin
   class ScriptSimilaritiesController < BaseController
     def index
+      @page = params[:page]&.to_i || 0
+      @page = 0 unless @page.between?(0, 100)
+
       @top_similarities = ScriptSimilarity
                           .joins(:script, :other_script)
                           .where(scripts: { script_delete_type_id: nil }, other_scripts_script_similarities: { script_delete_type_id: nil })
                           .order(similarity: :desc, script_id: :asc, other_script_id: :asc)
                           .limit(100)
+                          .offset(@page * 100)
       # This is slow
       # .includes(script: :localized_names, other_script: :localized_names)
 
--- app/models/spammy_email_domain.rb
+++ app/models/spammy_email_domain.rb
@@ -1,7 +1,7 @@
 class SpammyEmailDomain < ApplicationRecord
-  BLOCK_TYPE_CONFIRMATION = 'confirmation'
-  BLOCK_TYPE_SCRIPT = 'block_script'
-  BLOCK_TYPE_REGISTER = 'block_register'
+  BLOCK_TYPE_CONFIRMATION = 'confirmation'.freeze
+  BLOCK_TYPE_SCRIPT = 'block_script'.freeze
+  BLOCK_TYPE_REGISTER = 'block_register'.freeze
 
   def blocked_script_posting?
     [BLOCK_TYPE_SCRIPT, BLOCK_TYPE_REGISTER].include?(block_type)
@@ -9,9 +9,9 @@ class SpammyEmailDomain < ApplicationRecord
 
   def description
     case block_type
-    when BLOCK_TYPE_CONFIRMATION; 'Script posting delayed'
-    when BLOCK_TYPE_SCRIPT; "Can't post scripts"
-    when BLOCK_TYPE_REGISTER; "Can't register"
+    when BLOCK_TYPE_CONFIRMATION then 'Script posting delayed'
+    when BLOCK_TYPE_SCRIPT then "Can't post scripts"
+    when BLOCK_TYPE_REGISTER then "Can't register"
     end
   end
 end
