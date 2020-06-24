ff487655b54d66994b156cf56e862fdd0838b587
Jason Barnabe <jason.barnabe@gmail.com>
2020-06-23 21:33:27 -0500

    When converting a discussion, post in old discussion with URL #708

--- app/jobs/script_discussion_conversion_job.rb
+++ app/jobs/script_discussion_conversion_job.rb
@@ -11,6 +11,7 @@ class ScriptDiscussionConversionJob < ApplicationJob
       script.discussions.each do |forum_discussion|
         new_discussion = DiscussionConverter.convert(forum_discussion)
         new_discussion.save!
+        forum_discussion.forum_comments.create!(Body: "This discussion has been migrated to <a href=\"#{new_discussion.url}\"></a>", Format: 'Html', InsertUserID: 1, DateInserted: Time.now)
         forum_discussion.update_columns(Closed: 1)
       end
       script.update_attribute(:use_new_discussions, true)
