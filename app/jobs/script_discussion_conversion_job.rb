b3fc4d574abbf66416198d68bbc1dcd62ab6b3b8
Jason Barnabe <jason.barnabe@gmail.com>
2020-06-25 20:40:58 -0500

    Skip conversion of reports

--- app/jobs/script_discussion_conversion_job.rb
+++ app/jobs/script_discussion_conversion_job.rb
@@ -8,7 +8,7 @@ class ScriptDiscussionConversionJob < ApplicationJob
       script = Script.find(script_id)
       return if script.use_new_discussions
 
-      script.discussions.each do |forum_discussion|
+      script.discussions.where.not(rating: ForumDiscussion::RATING_REPORT).each do |forum_discussion|
         new_discussion = DiscussionConverter.convert(forum_discussion)
         new_discussion.save!
         forum_discussion.forum_comments.create!(Body: "This discussion has been migrated to <a href=\"#{new_discussion.url}\">#{new_discussion.url}</a>", Format: 'Html', InsertUserID: 1, DateInserted: Time.now)
