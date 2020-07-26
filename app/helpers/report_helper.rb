361bf1ea49480d49d9e00a4d97831c165b3ff974
Jason Barnabe <jason.barnabe@gmail.com>
2020-07-25 22:17:43 -0500

    Make who said what clearer in generic reports #741

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -7,4 +7,22 @@ module ReportHelper
       link_to "A comment by #{report.item.poster&.name || "Deleted user #{report.item.poster_id}"}", report.item.path
     end
   end
+
+  def reported_user(report)
+    case report.item
+    when User
+      report.item
+    when Comment
+      report.item.poster
+    end
+  end
+
+  def reported_user_id(report)
+    case report.item
+    when User
+      report.item.id
+    when Comment
+      report.item.poster_id
+    end
+  end
 end
