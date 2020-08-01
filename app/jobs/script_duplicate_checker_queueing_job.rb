bc75eb5b4fea0dc243ad8069eeb788313ac53765
Jason Barnabe <jason.barnabe@gmail.com>
2020-07-31 21:44:21 -0500

    Add logging to queuer

--- app/jobs/script_duplicate_checker_queueing_job.rb
+++ app/jobs/script_duplicate_checker_queueing_job.rb
@@ -3,9 +3,11 @@ class ScriptDuplicateCheckerQueueingJob < ApplicationJob
 
   def perform
     number_to_enqueue = ScriptDuplicateCheckerJob::DESIRED_RUN_COUNT - ScriptDuplicateCheckerJob.currently_queued_script_ids.count
+    Rails.logger.warn("We should enqueue #{number_to_enqueue} jobs.")
     return if number_to_enqueue <= 0
 
     script_ids = Rails.cache.fetch('ScriptDuplicateCheckerQueueingJob.queue') { [] }
+    Rails.logger.warn("Cached script IDs are: #{script_ids}")
 
     script_ids = calculate_script_ids if script_ids.empty?
 
@@ -13,6 +15,7 @@ class ScriptDuplicateCheckerQueueingJob < ApplicationJob
               .reject { |id| ScriptDuplicateCheckerJob.currently_queued_script_ids.include?(id) }
               .each { |id| ScriptDuplicateCheckerJob.perform_later(id) }
 
+    Rails.logger.warn("Caching script IDs: #{script_ids}")
     Rails.cache.write('ScriptDuplicateCheckerQueueingJob.queue', script_ids)
   end
 
