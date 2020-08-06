4d52fa44364782022fea768de9e5aceb22611e20
Jason Barnabe <jason.barnabe@gmail.com>
2020-08-05 19:44:46 -0500

    Throttle registrations by IP #758

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,13 @@
+SIGNUP_PATH_PATTERN = Regexp.new("\\A/(" + Rails.application.config.available_locales.keys.map { |locale| Regexp.escape(locale) }.join("|") + ")/users")
+
+Rack::Attack.throttle('limit registrations per ip', limit: 3, period: 3600) do |req|
+  if SIGNUP_PATH_PATTERN.match?(req.path) && req.post?
+    # Normalize the email, using the same logic as your authentication process, to
+    # protect against rate limit bypasses.
+    req.ip
+  end
+end if Rails.env.production?
+
+ActiveSupport::Notifications.subscribe("throttle.rack_attack") do |name, start, finish, request_id, payload|
+  Rails.logger.warn("#{payload[:request].env["rack.attack.matched"]} - #{payload[:request].ip}")
+end
