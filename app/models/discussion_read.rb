545157ca0866c505f559162f81c9970749af962a
Jason Barnabe <jason.barnabe@gmail.com>
2020-08-22 21:34:03 -0500

    Fix discussion not read after you post in it #778

--- app/models/discussion_read.rb
+++ app/models/discussion_read.rb
@@ -5,9 +5,9 @@ class DiscussionRead < ApplicationRecord
   def self.read_ids_for(discussions, user)
     ids = where(user: user, discussion_id: discussions.pluck(:id))
           .left_joins(:discussion)
-          .where('discussions.stat_last_reply_date < discussion_reads.read_at')
+          .where('discussions.stat_last_reply_date <= discussion_reads.read_at')
           .pluck(:discussion_id)
-    ids += discussions.where('discussions.stat_last_reply_date < ?', user.discussions_read_since).pluck(:id) if user.discussions_read_since
+    ids += discussions.where('discussions.stat_last_reply_date <= ?', user.discussions_read_since).pluck(:id) if user.discussions_read_since
     ids.to_set
   end
 end
