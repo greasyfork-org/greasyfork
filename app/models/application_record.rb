fb107b50cc5e8c9e8c3f377a4c581e60f7adb0df
Jason Barnabe <jason.barnabe@gmail.com>
2020-08-23 20:52:25 -0500

    Add a nil check in slugify

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,8 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def slugify(name)
+    return name if name.nil?
+
     # take out swears
     r = name.downcase.gsub(/motherfucking|motherfucker|fucking|fucker|fucks|fuck|shitty|shits|shit|niggers|nigger|cunts|cunt/, '')
     # multiple non-alphas into one
