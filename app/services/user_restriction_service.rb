9de376a29948e55c2a2143db16014116b4fa9f36
Jason Barnabe <jason.barnabe@gmail.com>
2020-09-01 22:21:13 -0500

    Expand rate limiting

--- app/services/user_restriction_service.rb
+++ app/services/user_restriction_service.rb
@@ -3,6 +3,12 @@ class UserRestrictionService
   BLOCKED = :blocked
   RATE_LIMITED = :rate_limited
 
+  NEW_USER_RATE_LIMITS = {
+    1.hour => 3,
+    1.day => 10,
+    1.week => 20,
+  }.freeze
+
   def initialize(user)
     @user = user
   end
@@ -18,12 +24,16 @@ class UserRestrictionService
 
     return NEEDS_CONFIRMATION unless @user.confirmed? || @user.identities.any?
 
-    return RATE_LIMITED if @user.created_at >= 1.hour.ago && @user.scripts.where('created_at >= ?', 1.hour.ago).count >= 3
+    return RATE_LIMITED if new_user? && NEW_USER_RATE_LIMITS.any? { |period, script_count| @user.scripts.where('created_at >= ?', period.ago).count >= script_count }
 
     nil
   end
 
   def must_recaptcha?
     @user.needs_to_recaptcha?
+  end
+
+  def new_user?
+    @user.created_at >= 1.month.ago && @user.scripts.not_deleted.where('created_at <= ?', 1.week.ago).none?
   end
 end
