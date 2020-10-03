2783ea4d15590c5b7acf42090ff7c61c30bc984c
Jason Barnabe <jason.barnabe@gmail.com>
2020-10-02 21:50:51 -0500

    No Adsense for Esperanto :(

--- app/controllers/concerns/shows_ads.rb
+++ app/controllers/concerns/shows_ads.rb
@@ -1,6 +1,6 @@
 module ShowsAds
   def choose_ad_method
-    return nil unless ads_enabled?
+    return nil unless ads_enabled? && locale_allows_adsense?
 
     'ga'
   end
@@ -11,9 +11,10 @@ module ShowsAds
 
   def choose_ad_method_for_script(script)
     return nil unless eligible_for_ads?(script)
-    return 'ca' if script.localized_attributes.where(attribute_key: 'additional_info').none?
 
-    script.adsense_approved ? 'ga' : %w[ca ea].sample
+    return 'ga' if script.adsense_approved && locale_allows_adsense? && script.localized_attributes.where(attribute_key: 'additional_info').any?
+
+    %w[ca ea].sample
   end
 
   def choose_ad_method_for_scripts(scripts)
@@ -33,6 +34,10 @@ module ShowsAds
     return false if sleazy?
 
     current_user.nil? || current_user.show_ads
+  end
+
+  def locale_allows_adsense?
+    !Rails.application.config.no_adsense_locales.include?(request_locale.code)
   end
 
   # FALLBACK_METHODS = ['ca', 'cf']
