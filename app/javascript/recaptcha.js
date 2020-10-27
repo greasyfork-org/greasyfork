4afe8f7ef724e76fba6a1c3818890733004ee9c4
Jason Barnabe <jason.barnabe@gmail.com>
2020-10-26 20:37:55 -0500

    Fix recaptcha functions

--- app/javascript/recaptcha.js
+++ app/javascript/recaptcha.js
@@ -1,16 +1,16 @@
-var submitInvisibleRecaptchaForm = function(event) {
+window.submitInvisibleRecaptchaForm = function(event) {
   document.getElementById("new_user").submit();
 };
-var submitInvisibleRecaptchaFormScriptVersion = function(event) {
+window.submitInvisibleRecaptchaFormScriptVersion = function(event) {
   document.getElementById("new_script_version").submit();
 };
-var submitInvisibleRecaptchaFormScriptSet = function(event) {
+window.submitInvisibleRecaptchaFormScriptSet = function(event) {
   document.getElementById("save-indicator").value = "1";
   document.getElementById("new_script_set").submit();
 };
-var submitInvisibleRecaptchaDiscussionForm = function(event) {
+window.submitInvisibleRecaptchaDiscussionForm = function(event) {
   document.getElementById("new-discussion").submit();
 };
-var submitInvisibleRecaptchaScriptDiscussionForm = function(event) {
+window.submitInvisibleRecaptchaScriptDiscussionForm = function(event) {
   document.getElementById("new-script-discussion").submit();
 }
