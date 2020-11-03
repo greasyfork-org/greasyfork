7afb14cea6f498383b9d0cc5fe88478a88c0786a
Jason Barnabe <jason.barnabe@gmail.com>
2020-11-03 09:35:20 -0600

    Handle user not existing in user checking job

--- app/jobs/user_checking_job.rb
+++ app/jobs/user_checking_job.rb
@@ -1,6 +1,8 @@
 class UserCheckingJob < ApplicationJob
   queue_as :low
 
+  discard_on ActiveJob::DeserializationError
+
   def perform(user)
     user.update(banned_at: Time.current) if user.name.include?('CX D K 58')
   end
