74f7b232eee07bb14fee23054022797151e5b1d1
Jason Barnabe <jason.barnabe@gmail.com>
2020-11-03 20:15:36 -0600

    Adjust anti-spam user detection

--- app/jobs/user_checking_job.rb
+++ app/jobs/user_checking_job.rb
@@ -4,6 +4,6 @@ class UserCheckingJob < ApplicationJob
   discard_on ActiveJob::DeserializationError
 
   def perform(user)
-    user.update(banned_at: Time.current) if user.name.include?('CX D K 58')
+    user.update(banned_at: Time.current) if user.name.include?('CX D K 58') || (user.email_domain == '163.com' && "#{user.name}@#{user.email_domain}" == user.email && /^[a-z]{3,5}[0-9]{3,5}$/.match?(user.name))
   end
 end
