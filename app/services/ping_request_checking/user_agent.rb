3db178885f9877baa494f18bcf513f17839c0650
Jason Barnabe <jason.barnabe@gmail.com>
2020-11-17 21:34:00 -0600

    Fix comparison in version

--- app/services/ping_request_checking/user_agent.rb
+++ app/services/ping_request_checking/user_agent.rb
@@ -4,7 +4,7 @@ module PingRequestChecking
   class UserAgent
     def self.check(request)
       user_agent = UserAgentParser.parse(request.headers['User-Agent'])
-      return user_agent.family != 'Chrome' || (user_agent.version.major && user_agent.version.major < 80)
+      return user_agent.family != 'Chrome' || (user_agent.version.major && user_agent.version.major.to_i < 80)
     end
   end
 end
