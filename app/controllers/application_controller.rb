55e4952b7cf41f1a69ac1c915af23e742c5a4092
Jason Barnabe <jason.barnabe@gmail.com>
2020-11-18 19:28:51 -0600

    Higher log level on cache misses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,9 +56,9 @@ class ApplicationController < ActionController::Base
     options[:version] = key.cache_version if key.respond_to?(:cache_version)
     key = "#{options.delete(:namespace)}/#{key.respond_to?(:cache_key) ? key.cache_key : key.to_s}" if options[:namespace]
     Rails.cache.fetch(key, options) do
-      Rails.logger.info("Cache miss - #{key} - #{options}") if Greasyfork::Application.config.log_cache_misses
+      Rails.logger.warn("Cache miss - #{key} - #{options}") if Greasyfork::Application.config.log_cache_misses
       o = yield
-      Rails.logger.info("Cache stored - #{key} - #{options}") if Greasyfork::Application.config.log_cache_misses
+      Rails.logger.warn("Cache stored - #{key} - #{options}") if Greasyfork::Application.config.log_cache_misses
       next o
     end
   end
