7a7f7f1013686e1bf3b065b1b018f1f3d367173b
Jason Barnabe <jason.barnabe@gmail.com>
2020-11-20 19:19:11 -0600

    Remove a workaround

--- app/jobs/banned_user_delete_job.rb
+++ app/jobs/banned_user_delete_job.rb
@@ -4,15 +4,7 @@ class BannedUserDeleteJob < ApplicationJob
   BANNED_AGE = 6.months
 
   def perform
-    # Can't do anything with Sphinx from within a Sidekiq job.
-    did_something = false
-    ThinkingSphinx::Callbacks.suspend do
-      User.where('banned_at < ?', BANNED_AGE.ago).find_each do |user|
-        user.destroy
-        did_something ||= true
-      end
-    end
-    SphinxReindexJob.perform_later if did_something
+    User.where('banned_at < ?', BANNED_AGE.ago).destroy_all
     self.class.set(wait: 1.hour).perform_later
   end
 end
--- app/jobs/sphinx_reindex_job.rb
+++
@@ -1,5 +0,0 @@
-class SphinxReindexJob < ApplicationJob
-  def perform
-    ThinkingSphinx::RakeInterface.new.sql.index
-  end
-end
