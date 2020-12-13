d8b4109d46c680fb7c9a7da38bc3c5f54687f771
Jason Barnabe <jason.barnabe@gmail.com>
2020-12-12 21:11:16 -0600

    Use 6.1 defaults

---
+++ config/initializers/queues.rb
@@ -0,0 +1,3 @@
+Rails.application.config.active_storage.queues.analysis = :active_storage_analysis
+Rails.application.config.active_storage.queues.purge = :active_storage_purge
+Rails.application.config.action_mailer.deliver_later_queue_name = :mailers
