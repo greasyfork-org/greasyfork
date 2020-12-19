ef5e3120551aa45f0dda5e28314bf9a8a9519ffb
Jason Barnabe <jason.barnabe@gmail.com>
2020-12-18 21:31:19 -0600

    Configure Active Storage for public access (#840)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.active_storage.service = :aws
+  config.active_storage.service = :public_aws
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
