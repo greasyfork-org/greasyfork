80e72839a401eba39f17f1fe937dd2496e6c9133
Jason Barnabe <jason.barnabe@gmail.com>
2021-01-22 19:37:03 -0600

    Markdown squiggles for strikethrough #867

--- app/helpers/user_text_helper.rb
+++ app/helpers/user_text_helper.rb
@@ -262,6 +262,6 @@ module UserTextHelper
   end
 
   def markdown
-    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new({ link_attributes: { rel: 'nofollow' } }), fenced_code_blocks: true, lax_spacing: true, tables: true)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new({ link_attributes: { rel: 'nofollow' } }), fenced_code_blocks: true, lax_spacing: true, tables: true, strikethrough: true)
   end
 end
