8084e9da41048da17479bb4105c63721c5ed25f4
Jason Barnabe <jason.barnabe@gmail.com>
2021-02-07 14:23:20 -0600

    Update references to mysql

--- shellscripts/update_fan_counts.sh
+++ shellscripts/update_fan_counts.sh
@@ -1,3 +1,3 @@
 echo "Starting at `date`" >> ../log/update_fan_counts.log
-mysql --login-path=greasyfork greasyfork < ./update_fan_counts.sql >> ../log/update_fan_counts.log 2>&1
+mysql < ./update_fan_counts.sql >> ../log/update_fan_counts.log 2>&1
 echo "Done at `date`" >> ../log/update_fan_counts.log
--- shellscripts/update_install_counts.sh
+++ shellscripts/update_install_counts.sh
@@ -1,3 +1,3 @@
 echo "Starting at `date`" >> ../log/update_install_counts.log
-mysql --login-path=greasyfork greasyfork < ./update_install_counts.sql >> ../log/update_install_counts.log 2>&1
+mysql < ./update_install_counts.sql >> ../log/update_install_counts.log 2>&1
 echo "Done at `date`" >> ../log/update_install_counts.log
