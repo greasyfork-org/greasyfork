0c7e2048c744048a26bc636d860fbabb12618d0a
Jason Barnabe <jason.barnabe@gmail.com>
2021-02-08 10:20:29 -0600

    Fix mysqlimport

--- shellscripts/update_counts_from_log.sh
+++ shellscripts/update_counts_from_log.sh
@@ -9,6 +9,6 @@ sed 's/\+[0-9][0-9]:[0-9][0-9]//' > /tmp/daily_update_check_counts.txt
 
 echo "Loading daily update counts into DB at `date`" >> ../log/update_counts_from_log.log
 
-mysqlimport --login-path=greasyfork --local --fields-terminated-by=" " --ignore greasyfork /tmp/daily_update_check_counts.txt >> ../log/update_counts_from_log.log 2>&1
+mysqlimport --local --fields-terminated-by=" " --ignore greasyfork /tmp/daily_update_check_counts.txt >> ../log/update_counts_from_log.log 2>&1
 
 echo "Done at `date`" >> ../log/update_counts_from_log.log
