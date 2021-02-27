76e0bfb908c971d7c78b3cb04c80d32f002c9e67
Jason Barnabe <jason.barnabe@gmail.com>
2021-02-27 16:49:35 -0600

    Update capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.14.1'
+lock '3.15.0'
 
 set :application, 'greasyfork'
 set :repo_url, 'https://github.com/JasonBarnabe/greasyfork.git'
