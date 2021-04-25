ce01d58532b07b1c5c8a4abb470617b45c2ef5d9
Jason Barnabe <jason.barnabe@gmail.com>
2021-04-25 15:56:43 -0500

    Adjust anti-crash guard

--- app/jobs/script_sync_job.rb
+++ app/jobs/script_sync_job.rb
@@ -5,7 +5,7 @@ class ScriptSyncJob < ApplicationJob
     script = Script.find(script_id)
 
     # Guard against script parsing crashing the process. If it fails 3 times, punt it for a month.
-    if script.sync_attempt_count == 3
+    if script.sync_attempt_count >= 3
       script.update(last_attempted_sync_date: 1.month.from_now, sync_attempt_count: 0)
       return
     end
