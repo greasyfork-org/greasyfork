d3fa00a1db9234556000b4db3659f93e433adff8
Jason Barnabe <jason.barnabe@gmail.com>
2021-05-06 14:23:24 -0500

    Adjust auto-banner

--- app/jobs/user_checking_job.rb
+++ app/jobs/user_checking_job.rb
@@ -4,6 +4,16 @@ class UserCheckingJob < ApplicationJob
   discard_on ActiveJob::DeserializationError
 
   def perform(user)
-    user.ban!(moderator: User.administrators.first, reason: 'Spam', delete_comments: true, delete_scripts: true) if user.name.include?('CX D K 58') || (user.email_domain == '163.com' && "#{user.name}@#{user.email_domain}" == user.email && /^[a-z]{3,5}[0-9]{3,5}$/.match?(user.name))
+    user.ban!(moderator: User.administrators.first, reason: 'Spam', delete_comments: true, delete_scripts: true) if bannable?(user)
+  end
+
+  def bannable?(user)
+    return true if user.name.include?('CX D K 58')
+    return true if user.email_domain == '163.com' && "#{user.name}@#{user.email_domain}" == user.email && /^[a-z]{3,5}[0-9]{3,5}$/.match?(user.name)
+    return true if user.email_domain == 'qq.com' && repeating?(user.email.split('@').first)
+  end
+
+  def repeating?(text)
+    text.length == 9 && text[0..2] == text[3..5] && text[6..8]
   end
 end
