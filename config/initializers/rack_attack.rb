b776ff99ef76a3a6f6e4458a4ed06d8f914abfea
Jason Barnabe <jason.barnabe@gmail.com>
2021-05-12 21:12:06 -0500

    Ban pentesters

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,12 @@
 SIGNUP_PATH_PATTERN = Regexp.new("\\A/(#{Rails.application.config.available_locales.keys.map { |locale| Regexp.escape(locale) }.join('|')})/users\\z")
 LOGIN_PATH_PATTERN = Regexp.new("\\A/(#{Rails.application.config.available_locales.keys.map { |locale| Regexp.escape(locale) }.join('|')})/users/sign_in\\z")
 
+PENTESTER_QUERY_STRINGS = [
+  '/etc/passwd',
+  'bxss.me',
+  'gethostbyname'
+]
+
 if Rails.env.production?
   if Rails.application.config.ip_address_tracking
     Rack::Attack.throttle('limit registrations per ip', limit: 3, period: 3600) do |req|
@@ -21,6 +27,15 @@ if Rails.env.production?
   if Rails.application.config.ip_address_tracking
     Rack::Attack.throttle('limit logins attempts per ip', limit: 20, period: 3600) do |req|
       req.ip if LOGIN_PATH_PATTERN.match?(req.path) && req.post? && req.params.dig('user', 'email')
+    end
+  end
+
+  if Rails.application.config.ip_address_tracking
+    Rack::Attack.blocklist('fail2ban pentesters') do |req|
+      Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 5.minutes) do
+        qs = CGI.unescape(req.query_string)
+        PENTESTER_QUERY_STRINGS.any? { |pentester_qs| qs.include?(pentester_qs) }
+      end
     end
   end
 end
