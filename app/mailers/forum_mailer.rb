1ef8022b3de2177cb7151cf21e93cf8cabc3144e
Jason Barnabe <jason.barnabe@gmail.com>
2021-08-14 21:29:36 -0500

    HTML format for forum notifications #967

--- app/mailers/forum_mailer.rb
+++ app/mailers/forum_mailer.rb
@@ -1,58 +1,63 @@
 class ForumMailer < ApplicationMailer
-  include UserTextHelper
   include UsersHelper
+  helper UsersHelper
+  helper UserTextHelper
 
   def comment_on_script(author_user, comment)
-    site_name = 'Greasy Fork'
-    locale = author_user.available_locale_code
-    mail(to: author_user.email, subject: t('mailers.script_comment.subject', script_name: comment.script.name(locale), site_name: site_name, locale: locale)) do |format|
-      format.text do
-        render plain: t('mailers.script_comment.text',
-                        script_name: comment.script.name(locale),
-                        site_name: site_name,
-                        script_url: script_url(comment.script, locale: locale),
-                        commenter_name: render_user_text(comment.poster, comment.poster_id),
-                        comment_text: format_user_text_as_plain(comment.text, comment.text_markup, use_line_breaks: true),
-                        comment_url: comment.url,
-                        notification_preferences_url: notifications_user_url(author_user, locale: locale),
-                        locale: locale)
-      end
-    end
+    @comment = comment
+    @site_name = 'Greasy Fork'
+    @receiving_user = author_user
+    I18n.locale = @locale = @receiving_user.available_locale_code
+
+    mail(
+      to: @receiving_user.email,
+      subject: t('mailers.script_comment.subject',
+                 script_name: @comment.script.name(@locale),
+                 site_name: @site_name,
+                 locale: @locale)
+    )
   end
 
   def comment_on_subscribed(user, comment)
-    site_name = 'Greasy Fork'
-    locale = user.available_locale_code
-    mail(to: user.email, subject: t('mailers.subscribed_discussion.subject', discussion_title: comment.discussion.display_title(locale: locale), site_name: site_name, locale: locale)) do |format|
-      format.text do
-        render plain: t('mailers.subscribed_discussion.text',
-                        discussion_title: comment.discussion.display_title(locale: locale),
-                        site_name: site_name,
-                        commenter_name: comment.poster.name,
-                        comment_text: format_user_text_as_plain(comment.text, comment.text_markup, use_line_breaks: true),
-                        comment_url: comment.url,
-                        discussion_url: comment.discussion.url,
-                        notification_preferences_url: notifications_user_url(user, locale: locale),
-                        locale: locale)
-      end
-    end
+    @comment = comment
+    @site_name = 'Greasy Fork'
+    @receiving_user = user
+    I18n.locale = @locale = @receiving_user.available_locale_code
+
+    mail(
+      to: user.email,
+      subject: t('mailers.subscribed_discussion.subject',
+                 site_name: @site_name,
+                 locale: @locale,
+                 **localization_params_for_comment(@comment, @locale))
+    )
   end
 
   def comment_on_mentioned(user, comment)
-    site_name = 'Greasy Fork'
-    locale = user.available_locale_code
-    mail(to: user.email, subject: t('mailers.comment_mentioned.subject', discussion_title: comment.discussion.display_title(locale: locale), site_name: site_name, locale: locale, commenter_name: comment.poster.name)) do |format|
-      format.text do
-        render plain: t('mailers.comment_mentioned.text',
-                        discussion_title: comment.discussion.display_title(locale: locale),
-                        site_name: site_name,
-                        commenter_name: comment.poster.name,
-                        comment_text: format_user_text_as_plain(comment.text, comment.text_markup), use_line_breaks: true,
-                        comment_url: comment.url,
-                        discussion_url: comment.discussion.url,
-                        notification_preferences_url: notifications_user_url(user, locale: locale),
-                        locale: locale)
-      end
-    end
+    @comment = comment
+    @site_name = 'Greasy Fork'
+    @receiving_user = user
+    I18n.locale = @locale = user.available_locale_code
+
+    mail(
+      to: user.email,
+      subject: t('mailers.comment_mentioned.subject',
+                 site_name: @site_name,
+                 locale: @locale,
+                 **localization_params_for_comment(@comment, @locale))
+    )
   end
+
+  protected
+
+  def localization_params_for_comment(comment, locale)
+    {
+      commenter_link: user_url(comment.poster_id, locale: locale),
+      commenter_name: render_user_text(comment.poster, comment.poster_id),
+      comment_link: comment.url,
+      discussion_title: comment.discussion.display_title(locale: locale),
+      discussion_link: comment.discussion.url,
+    }
+  end
+  helper_method(:localization_params_for_comment)
 end
