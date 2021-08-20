be0ce0059b38c781e2cbbe059d2335453b9b3878
Jason Barnabe <jason.barnabe@gmail.com>
2021-08-19 19:08:08 -0500

    HTML notifcations for messages

--- app/mailers/conversation_mailer.rb
+++ app/mailers/conversation_mailer.rb
@@ -1,35 +1,32 @@
 class ConversationMailer < ApplicationMailer
-  include UserTextHelper
+  include UsersHelper
+  helper UsersHelper
+  helper UserTextHelper
 
-  def new_conversation(conversation, to_user, initiator_user)
-    site_name = 'Greasy Fork'
-    locale = to_user.available_locale_code
-    mail(to: to_user.email, subject: t('mailers.new_message.subject', site_name: site_name, locale: locale, user: initiator_user.name)) do |format|
-      format.text do
-        render plain: t('mailers.new_message.text',
-                        user: initiator_user.name,
-                        conversation_url: user_conversation_url(to_user, conversation, locale: locale),
-                        site_name: site_name,
-                        locale: locale,
-                        notification_preferences_url: notifications_user_url(to_user, locale: locale),
-                        message_text: format_user_text_as_plain(conversation.messages.first.content, conversation.messages.first.content_markup, use_line_breaks: true))
-      end
-    end
+  def new_conversation(conversation, receiving_user, initiator_user)
+    @message = conversation.messages.first
+    @receiving_user = receiving_user
+    @site_name = 'Greasy Fork'
+    I18n.locale = @locale = @receiving_user.available_locale_code
+    @conversation_url = user_conversation_url(@receiving_user, conversation, locale: @locale)
+
+    mail(
+      to: @receiving_user.email,
+      subject: t('mailers.new_message.subject', site_name: @site_name, locale: @locale, user: initiator_user.name),
+      template_name: 'new_message'
+    )
   end
 
-  def new_message(message, to_user)
-    site_name = 'Greasy Fork'
-    locale = to_user.available_locale_code
-    mail(to: to_user.email, subject: t('mailers.new_message.subject', site_name: site_name, locale: locale, user: message.poster.name)) do |format|
-      format.text do
-        render plain: t('mailers.new_message.text',
-                        user: message.poster.name,
-                        site_name: site_name,
-                        conversation_url: user_conversation_url(to_user, message.conversation, locale: locale, anchor: "message-#{message.id}"),
-                        locale: locale,
-                        notification_preferences_url: notifications_user_url(to_user, locale: locale),
-                        message_text: format_user_text_as_plain(message.content, message.content_markup, use_line_breaks: true))
-      end
-    end
+  def new_message(message, receiving_user)
+    @message = message
+    @receiving_user = receiving_user
+    @site_name = 'Greasy Fork'
+    I18n.locale = @locale = @receiving_user.available_locale_code
+    @conversation_url = user_conversation_url(@receiving_user, message.conversation, locale: @locale, anchor: "message-#{message.id}")
+
+    mail(
+      to: @receiving_user.email,
+      subject: t('mailers.new_message.subject', site_name: @site_name, locale: @locale, user: message.poster.name)
+    )
   end
 end
