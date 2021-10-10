6496329a3f3493bc8bc075069c42c99d697e8a7f
Jason Barnabe <jason.barnabe@gmail.com>
2021-10-10 13:12:43 -0500

    Add another server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 server '192.53.127.123', user: 'deploy', roles: %w[app db web]
 server '45.56.87.231', user: 'deploy', roles: %w[app web]
+server '45.79.87.224', user: 'deploy', roles: %w[app web]
 server '45.33.40.51', user: 'deploy', roles: %w[worker]
