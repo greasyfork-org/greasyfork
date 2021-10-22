7f14ed0b6cc0fce46cfd3dbae8630d3440092f60
Jason Barnabe <jason.barnabe@gmail.com>
2021-10-21 21:18:06 -0500

    Handle CSRF token in cached pages

--- app/controllers/concerns/page_cache.rb
+++ app/controllers/concerns/page_cache.rb
@@ -1,12 +1,21 @@
 module PageCache
-  def cache_page(page_key, &block)
+  CSRF_META_TAGS = '<!-- csrf_meta_tags -->'.html_safe
+  CSRF_TOKEN = '__CSRF_TOKEN__'.freeze
+
+  def cache_page(page_key)
     if page_key.nil? || current_user
       html = yield
       render html: html
       return
     end
 
-    html = Rails.cache.fetch(page_key, expires_in: 1.minute, &block)
+    html = Rails.cache.fetch(page_key, expires_in: 1.minute) do
+      @caching_request = true
+      yield
+    end
     render html: html
+      .gsub(CSRF_META_TAGS, view_context.csrf_meta_tags)
+      .gsub(CSRF_TOKEN, session[:_csrf_token])
+      .html_safe
   end
 end
