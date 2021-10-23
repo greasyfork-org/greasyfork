7347132267689e03d02e66b386d7e6dcd2a361e7
Jason Barnabe <jason.barnabe@gmail.com>
2021-10-22 19:44:04 -0500

    Fix ip address on cached pages

--- app/controllers/concerns/page_cache.rb
+++ app/controllers/concerns/page_cache.rb
@@ -1,6 +1,7 @@
 module PageCache
   CSRF_META_TAGS = '<!-- csrf_meta_tags -->'.html_safe
   CSRF_TOKEN = '__CSRF_TOKEN__'.freeze
+  IP_ADDRESS = '__IP_ADDRESS__'.freeze
 
   def cache_page(page_key)
     if page_key.nil? || current_user
@@ -19,6 +20,7 @@ module PageCache
     render html: html
       .gsub(CSRF_META_TAGS, view_context.csrf_meta_tags)
       .gsub(CSRF_TOKEN, session[:_csrf_token])
+      .gsub(IP_ADDRESS, request.remote_ip)
       .html_safe
   end
 end
