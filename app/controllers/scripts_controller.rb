aca174657ecbd4afadc29047406fb640ed42a4f6
Jason Barnabe <jason.barnabe@gmail.com>
2021-11-11 17:02:25 -0600

    Include tersed in script similarity results #995

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -591,12 +591,12 @@ class ScriptsController < ApplicationController
     # Include the inverse as well so that we can notice new/updated scripts that are similar to this. If we have the
     # relation both forward and backward, take the higher score.
     @similarities = []
-    @script.script_similarities.includes(:other_script).where(similarity: DERIVATIVE_SCORES.map(&:last).min.., tersed: false).order(similarity: :desc, id: :asc).each do |ss|
+    @script.script_similarities.includes(:other_script).where(similarity: DERIVATIVE_SCORES.map(&:last).min..).order(similarity: :desc, id: :asc).each do |ss|
       @similarities << [ss.other_script, ss.similarity] if current_user&.moderator? || !ss.other_script.deleted?
     end
     # If we haven't run the forward, don't bother with the backward.
     if @similarities.any?
-      ScriptSimilarity.where(other_script: @script).where(similarity: DERIVATIVE_SCORES.map(&:last).min.., tersed: false).order(similarity: :desc, id: :asc).each do |ss|
+      ScriptSimilarity.where(other_script: @script).where(similarity: DERIVATIVE_SCORES.map(&:last).min..).order(similarity: :desc, id: :asc).each do |ss|
         @similarities << [ss.script, ss.similarity] if current_user&.moderator? || !ss.script.deleted?
       end
       @similarities = @similarities.sort_by(&:last).reverse.uniq(&:first).first(100)
