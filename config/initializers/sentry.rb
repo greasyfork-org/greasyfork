58ce403e8fff45a13fb97c86c175cadb6c588955
Jason Barnabe <jason.barnabe@gmail.com>
2021-11-13 15:27:11 -0600

    Ignore JSON::ParserError in Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,5 @@
 Sentry.init do |config|
   config.dsn = Rails.application.secrets.sentry_api_key
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.excluded_exceptions += ['JSON::ParserError']
 end
