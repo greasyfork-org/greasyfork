1eed45f6dd9cf47277425b559936debf95b223e6
Jason Barnabe <jason.barnabe@gmail.com>
2021-11-14 13:00:47 -0600

    Add max length validation to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,7 +11,7 @@ class Comment < ApplicationRecord
   belongs_to :poster, class_name: 'User', optional: true
   has_many :reports, as: :item, dependent: :destroy
 
-  validates :text, presence: true
+  validates :text, presence: true, length: { maximum: 65_535 }
   validates :text_markup, inclusion: { in: %w[html markdown] }, presence: true
 
   delegate :script, :discussion_category, to: :discussion
