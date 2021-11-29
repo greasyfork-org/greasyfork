d47f08827398416cb64354d894e3ef67403cc345
Jason Barnabe <jason.barnabe@gmail.com>
2021-11-29 17:35:59 -0600

    Rubocop

--- app/controllers/concerns/shows_ads.rb
+++ app/controllers/concerns/shows_ads.rb
@@ -12,10 +12,10 @@ module ShowsAds
   def choose_ad_method_for_script(script)
     return nil unless eligible_for_ads?(script)
 
-    #return 'vi' if script.id == 1
+    # return 'vi' if script.id == 1
     return 'ga' if script.adsense_approved && locale_allows_adsense? && script.localized_attributes.where(attribute_key: 'additional_info').any?
 
-    #return 'vi' if [:'zh-CN', :'zh-TW'].include?(I18n.locale) && script.id.even?
+    # return 'vi' if [:'zh-CN', :'zh-TW'].include?(I18n.locale) && script.id.even?
 
     %w[ca ea].sample
   end
