4c0fa4efb2de01d61eb683f6afb0935c5bb07c2e
Jason Barnabe <jason.barnabe@gmail.com>
2021-12-26 20:50:13 -0600

    Load Rails 7 defaults

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,7 @@ Bundler.require(*Rails.groups)
 module Greasyfork
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
-    config.active_support.cache_format_version = 6.1
+    config.load_defaults 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
