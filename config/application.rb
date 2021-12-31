8258e4dce646db3de4fa5ab369d0528c30d3c87f
Jason Barnabe <jason.barnabe@gmail.com>
2021-12-30 18:58:31 -0600

    Update cache_format_version

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,6 @@ module Greasyfork
     # https://github.com/rails/rails/issues/44005
     config.active_support.executor_around_test_case = false
 
-    config.active_support.cache_format_version = 6.1
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
