7222941bbf51d88c1580734547f5447e0db37eeb
Jason Barnabe <jason.barnabe@gmail.com>
2022-02-20 15:29:28 -0600

    Make Sentry ignore some exceptions

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
   config.dsn = Rails.application.secrets.sentry_api_key
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.excluded_exceptions += ['JSON::ParserError']
+  config.excluded_exceptions += ['JSON::ParserError', 'Sidekiq::JobRetry::Skip', 'ThinkingSphinx::SphinxError']
 end
