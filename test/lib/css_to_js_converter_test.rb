4275916620429d579c07b7fa3652fc8743920a2f
Jason Barnabe <jason.barnabe@gmail.com>
2022-03-21 19:11:00 -0500

    Adjust conversion of regexps from CSS to JS #1055

--- test/lib/css_to_js_converter_test.rb
+++ test/lib/css_to_js_converter_test.rb
@@ -285,6 +285,112 @@ class CssToJsConverterTest < ActiveSupport::TestCase
     assert_equal js, CssToJsConverter.convert(css)
   end
 
+  test 'js conversion with regexp' do
+    css = <<~CSS
+      /* ==UserStyle==
+      @name        Example UserCSS style
+      @namespace   github.com/openstyles/stylus
+      @version     1.0.0
+      @license     unlicense
+      ==/UserStyle== */
+
+      @-moz-document regexp("https?://www\\.google\\.com") {
+        a {
+          color: red;
+        }
+      }
+    CSS
+
+    js = <<~JS
+      // ==UserScript==
+      // @name Example UserCSS style
+      // @namespace github.com/openstyles/stylus
+      // @version 1.0.0
+      // @license unlicense
+      // @grant GM_addStyle
+      // @run-at document-start
+      // @include /^https?://www\\.google\\.com$/
+      // ==/UserScript==
+
+      (function() {
+      let css = `
+        a {
+          color: red;
+        }
+      `;
+      if (typeof GM_addStyle !== "undefined") {
+        GM_addStyle(css);
+      } else {
+        let styleNode = document.createElement("style");
+        styleNode.appendChild(document.createTextNode(css));
+        (document.querySelector("head") || document.documentElement).appendChild(styleNode);
+      }
+      })();
+    JS
+    assert_equal js, CssToJsConverter.convert(css)
+  end
+
+  test 'js conversion of multi blocked regexp' do
+    css = <<~CSS
+      /* ==UserStyle==
+      @name        Example UserCSS style
+      @namespace   github.com/openstyles/stylus
+      @version     1.0.0
+      @license     unlicense
+      ==/UserStyle== */
+
+      @-moz-document regexp("http://example\\.com/") {
+        a {
+          color: red;
+        }
+      }
+      @-moz-document regexp("http://example\\.net/") {
+        a {
+          color: blue;
+        }
+      }
+    CSS
+
+    js = <<~JS
+      // ==UserScript==
+      // @name Example UserCSS style
+      // @namespace github.com/openstyles/stylus
+      // @version 1.0.0
+      // @license unlicense
+      // @grant GM_addStyle
+      // @run-at document-start
+      // @include /^http://example\\.com/$/
+      // @include /^http://example\\.net/$/
+      // ==/UserScript==
+
+      (function() {
+      let css = "";
+      if (new RegExp("^http://example\\\\.com/$").test(location.href)) {
+        css += `
+          a {
+            color: red;
+          }
+        `;
+      }
+      if (new RegExp("^http://example\\\\.net/$").test(location.href)) {
+        css += `
+          a {
+            color: blue;
+          }
+        `;
+      }
+      if (typeof GM_addStyle !== "undefined") {
+        GM_addStyle(css);
+      } else {
+        let styleNode = document.createElement("style");
+        styleNode.appendChild(document.createTextNode(css));
+        (document.querySelector("head") || document.documentElement).appendChild(styleNode);
+      }
+      })();
+    JS
+    assert_equal js, CssToJsConverter.convert(css)
+  end
+
   test 'calculate_includes domain with overlapping URL' do
     block = CssParser::CssDocumentBlock.new([
                                               CssParser::CssDocumentMatch.new('domain', 'example.com'),
