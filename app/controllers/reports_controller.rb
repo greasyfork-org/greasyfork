581fe408bfaa3a2592f658ef68dc26e3a9b82959
Jason Barnabe <jason.barnabe@gmail.com>
2022-04-20 20:41:22 -0500

    Fix marking discussions as requiring review because of trusted users

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -30,8 +30,8 @@ class ReportsController < ApplicationController
     end
 
     if current_user.trusted_reports
-      item.update!(Discussion::REVIEW_REASON_TRUSTED) if @report.item.is_a?(Discussion) && @report.reason != Report::REASON_WRONG_CATEGORY
-      item.discussion.update!(Discussion::REVIEW_REASON_TRUSTED) if @report.item.is_a?(Comment) && @report.item.first_comment?
+      item.update!(review_reason: Discussion::REVIEW_REASON_TRUSTED) if @report.item.is_a?(Discussion) && @report.reason != Report::REASON_WRONG_CATEGORY
+      item.discussion.update!(review_reason: Discussion::REVIEW_REASON_TRUSTED) if @report.item.is_a?(Comment) && @report.item.first_comment?
     end
 
     ScriptReportMailer.report_created(@report, site_name).deliver_later if @report.item.is_a?(Script)
