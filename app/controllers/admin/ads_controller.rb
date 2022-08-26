77f5812f96ccac34b86b5a39f04d861d3277b011
Jason Barnabe <jason.barnabe@gmail.com>
2022-08-25 20:25:13 -0500

    Prevent ad-approving scripts with certain keywords

--- app/controllers/admin/ads_controller.rb
+++ app/controllers/admin/ads_controller.rb
@@ -1,13 +1,14 @@
 module Admin
   class AdsController < BaseController
     DISALLOWED_INCLUDES = %w[youtube google].freeze
+    DISALLOWED_KEYWORDS = (%w[hack crack drawing] + DISALLOWED_INCLUDES).freeze
 
     before_action :administrators_only
 
     def pending
       @scripts = Script
                  .active(:greasyfork)
-                 .includes(:site_applications)
+                 .includes(:site_applications, :localized_attributes)
                  .where(adsense_approved: nil)
                  .where.not(script_type_id: ScriptType::LIBRARY_TYPE_ID)
                  .order(page_views: :desc)
@@ -23,5 +24,16 @@ module Admin
       Script.find(params[:id]).update(adsense_approved: false)
       redirect_to pending_admin_ads_path
     end
+
+    private
+
+    def disallowed_keyword_regexp
+      @_disallowed_keyword_regexp ||= Regexp.new(Regexp.union(DISALLOWED_KEYWORDS.map {|keyword| Regexp.escape(keyword)}), true)
+    end
+
+    def contains_disallowed_keyword?(script)
+      script.localized_attributes.any? {|la| disallowed_keyword_regexp.match?(la.attribute_value) }
+    end
+    helper_method :contains_disallowed_keyword?
   end
 end
