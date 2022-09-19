d36222aa9c8020d9b4f87a674ac30f0dd971d2f8
Jason Barnabe <jason.barnabe@gmail.com>
2022-09-18 20:26:08 -0500

    Remove duplicates in subresource check job #1070

--- app/jobs/subresource_check_queueing_job.rb
+++ app/jobs/subresource_check_queueing_job.rb
@@ -6,6 +6,7 @@ class SubresourceCheckQueueingJob < ApplicationJob
       .with_integrity_hash_usages
       .where('last_attempt_at IS NULL or last_attempt_at < ?', 1.day.ago)
       .order(:last_attempt_at)
+      .distinct
       .limit(50)
       .each do |subresource|
       SubresourceCheckJob.perform_later(subresource)
