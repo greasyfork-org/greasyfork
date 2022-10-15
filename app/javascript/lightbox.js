c762d00e7968de9dcdb3e029c8bacc94c0a864f0
Jason Barnabe <jason.barnabe@gmail.com>
2022-10-15 11:35:48 -0500

    Lazily load lightbox

--- app/javascript/lightbox.js
+++ app/javascript/lightbox.js
@@ -1,16 +1,17 @@
-import { Luminous, LuminousGallery } from 'luminous-lightbox';
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".remove-attachments, .user-screenshots").forEach((el) => {
-    let imageLinks = el.querySelectorAll("a")
-    switch (imageLinks.length) {
-      case 0:
-        break;
-      case 1:
-        new Luminous(imageLinks[0]);
-        break;
-      default:
-        new LuminousGallery(imageLinks);
-    }
-  })
+document.addEventListener("DOMContentLoaded", async () => {
+  if (document.querySelector(".remove-attachments, .user-screenshots")) {
+    const { Luminous, LuminousGallery } = await import('luminous-lightbox');
+    document.querySelectorAll(".remove-attachments, .user-screenshots").forEach((el) => {
+      let imageLinks = el.querySelectorAll("a")
+      switch (imageLinks.length) {
+        case 0:
+          break;
+        case 1:
+          new Luminous(imageLinks[0]);
+          break;
+        default:
+          new LuminousGallery(imageLinks);
+      }
+    })
+  }
 })
