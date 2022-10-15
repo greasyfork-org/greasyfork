522188d503111d986e0f15c8b4cc04b61a621ad6
Jason Barnabe <jason.barnabe@gmail.com>
2022-10-15 13:58:36 -0500

    gzip assets with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
   plugins: [
-    RubyPlugin(),
+    RubyPlugin(), viteCompression()
   ],
 })
