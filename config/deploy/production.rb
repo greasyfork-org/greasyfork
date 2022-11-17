4147e2f4703b698b51b8687032e86353d43f42dd
Jason Barnabe <jason.barnabe@gmail.com>
2022-11-16 20:00:16 -0600

    Remove old worker from deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,4 @@
 server '192.53.127.123', user: 'deploy', roles: %w[app db web]
 server '45.56.87.231', user: 'deploy', roles: %w[app web]
 server '45.79.87.224', user: 'deploy', roles: %w[app web]
-server '45.33.40.51', user: 'deploy', roles: %w[worker]
 server '192.155.83.16', user: 'deploy', roles: %w[worker]
