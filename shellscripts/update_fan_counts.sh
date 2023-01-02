60c34b818359c4e65852237d626eb1e0c39c6de2
Jason Barnabe <jason.barnabe@gmail.com>
2023-01-02 14:15:36 -0600

    Reindex after updating install counts

--- shellscripts/update_fan_counts.sh
+++ shellscripts/update_fan_counts.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+MAILTO="jason.barnabe@gmail.com"
+
 echo "Starting at `date`" >> ../log/update_fan_counts.log
 mysql < ./update_fan_counts.sql >> ../log/update_fan_counts.log 2>&1
+# Should also reindex after this, but we let update_install_counts.sh do it as it finishes after us.
 echo "Done at `date`" >> ../log/update_fan_counts.log
--- shellscripts/update_install_counts.sh
+++ shellscripts/update_install_counts.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+MAILTO="jason.barnabe@gmail.com"
+
 echo "Starting at `date`" >> ../log/update_install_counts.log
 mysql < ./update_install_counts.sql >> ../log/update_install_counts.log 2>&1
+echo "Reindexing at `date`" >> ../log/update_install_counts.log
+RAILS_ENV=production /home/deploy/.rbenv/bin/rbenv exec bundle exec rake ts:index >> ../log/update_install_counts.log 2>&1
 echo "Done at `date`" >> ../log/update_install_counts.log
