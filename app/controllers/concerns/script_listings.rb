133f7f7e22f0eebc072d27172696ba8898c5161d
Jason Barnabe <jason.barnabe@gmail.com>
2023-03-28 22:15:49 -0500

    Allow caching script listings with sort

--- app/controllers/concerns/script_listings.rb
+++ app/controllers/concerns/script_listings.rb
@@ -24,7 +24,7 @@ module ScriptListings
 
     respond_to do |format|
       format.html do
-        should_cache_page = current_user.nil? && request.format.html? && (params.keys - %w[locale controller action site page]).none?
+        should_cache_page = current_user.nil? && request.format.html? && (params.keys - %w[locale controller action site page sort]).none?
         cache_page(should_cache_page ? "script_index/#{params.values.join('/')}" : nil) do
           status = 200
 
