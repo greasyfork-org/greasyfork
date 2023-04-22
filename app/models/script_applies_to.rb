315f19c92581fb12ed10ba5adb03d1fa318256ee
Jason Barnabe <jason.barnabe@gmail.com>
2023-04-21 19:32:59 -0500

    Script checkup

--- app/models/script_applies_to.rb
+++ app/models/script_applies_to.rb
@@ -2,5 +2,5 @@ class ScriptAppliesTo < ApplicationRecord
   belongs_to :script
   belongs_to :site_application
 
-  delegate :text, :domain, to: :site_application
+  delegate :text, :domain, :domain?, to: :site_application
 end
