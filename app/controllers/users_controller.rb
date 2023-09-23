c3d43e96b46e8f4ca4aa8817160c874d5e8754de
Jason Barnabe <jason.barnabe@gmail.com>
2023-09-23 16:05:33 -0500

    Remove unnecessary includes on users/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,15 +73,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    # TODO: sort scripts by name, keeping into account localization
-    user = User.order('scripts.default_name')
-    # current user will display discussions
-    user = if !current_user.nil? && (current_user.id == params[:id].to_i)
-             user.includes(scripts: [:discussions, { localized_attributes: :locale }])
-           else
-             user.includes(scripts: { localized_attributes: :locale })
-           end
-    @user = user.find(params[:id])
+    @user = User.find(params[:id])
 
     return if redirect_to_slug(@user, :id)
 
