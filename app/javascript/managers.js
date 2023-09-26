c47c86e829d5ec691a432a0d03212bdeb8ce1cef
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2023-09-26 14:48:36 +0900

    Make StylusDetection Faster (1)

--- app/javascript/managers.js
+++ app/javascript/managers.js
@@ -11,25 +11,29 @@ export function canInstallUserJS() {
 }
 
 export async function canInstallUserCSS() {
-  if (localStorage.getItem('stylusDetected') === '1') {
+  if (localStorage.getItem('stylusDetected') == '1') {
     return true;
   }
+
+  let messageListener;
+
   await new Promise(resolve => {
-    window.stylusDetectedResolve = resolve;
+    messageListener = function(event) {
+      if (event.origin !== location.origin)
+        return;
+  
+      if (event.data.type != "style-version")
+        return;
+  
+      localStorage.setItem('stylusDetected', '1')
+  
+      resolve();
+    };
+    window.addEventListener("message", messageListener, false);
     postMessage({ type: 'style-version-query', name: "whatever", namespace: "whatever", url: location.href }, location.origin);
     setTimeout(resolve, 200);
   });
-  window.stylusDetectedResolve = null;
-  return localStorage.getItem('stylusDetected') === '1';
+
+  window.removeEventListener("message", messageListener, false);
+  return localStorage.getItem('stylusDetected') == '1';
 }
-
-window.addEventListener("message", function (event) {
-  if (event.origin !== location.origin)
-    return;
-
-  if (event.data.type === "style-version") {
-    localStorage.setItem('stylusDetected', '1');
-    if (window.stylusDetectedResolve) window.stylusDetectedResolve();
-  }
-
-})
