c8990c53a71e79239bf00c243521f22bfd06849d
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2023-10-03 11:12:03 +0900

    Make StylusDetection Faster (#1165)
    The `200ms` shall not be necessary since the `Promise` can be directly resolved by `onMessage`

--- app/javascript/managers.js
+++ app/javascript/managers.js
@@ -14,18 +14,26 @@ export async function canInstallUserCSS() {
   if (localStorage.getItem('stylusDetected') == '1') {
     return true;
   }
-  postMessage({ type: 'style-version-query', name: "whatever", namespace: "whatever", url: location.href }, location.origin);
-  return new Promise(resolve => setTimeout(function() {
-    resolve(localStorage.getItem('stylusDetected') == '1')
-  }, 200));
+
+  let messageListener;
+
+  await new Promise(resolve => {
+    messageListener = function(event) {
+      if (event.origin !== location.origin)
+        return;
+  
+      if (event.data.type != "style-version")
+        return;
+  
+      localStorage.setItem('stylusDetected', '1')
+  
+      resolve();
+    };
+    window.addEventListener("message", messageListener, false);
+    postMessage({ type: 'style-version-query', name: "whatever", namespace: "whatever", url: location.href }, location.origin);
+    setTimeout(resolve, 200);
+  });
+
+  window.removeEventListener("message", messageListener, false);
+  return localStorage.getItem('stylusDetected') == '1';
 }
-
-window.addEventListener("message", function(event) {
-  if (event.origin !== location.origin)
-    return;
-
-  if (event.data.type != "style-version")
-    return;
-
-  localStorage.setItem('stylusDetected', '1')
-})
