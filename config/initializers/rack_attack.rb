ee8c5a2202ddc6b388a5fef2c6a9d7d0923fcda4
Jason Barnabe <jason.barnabe@gmail.com>
2023-11-17 19:21:33 -0600

    Adjust patch

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,22 +1,20 @@
 # https://github.com/rack/rack-attack/issues/210#issuecomment-786859918
-module Rack
-  module Attack
-    class Throttle
-      def exceeded?(request)
-        discriminator = discriminator_for(request)
-        return false unless discriminator
+module RackAttackThrottleExceeded
+  def exceeded?(request)
+    discriminator = discriminator_for(request)
+    return false unless discriminator
 
-        current_period = period_for(request)
-        current_limit = limit_for(request)
+    current_period = period_for(request)
+    current_limit = limit_for(request)
 
-        key = [Time.now.to_i / current_period, name, discriminator].join(':')
-        count = cache.read(key).to_i
+    key = [Time.now.to_i / current_period, name, discriminator].join(':')
+    count = cache.read(key).to_i
 
-        count > current_limit
-      end
-    end
+    count > current_limit
   end
 end
+
+Rack::Attack::Throttle.include(RackAttackThrottleExceeded)
 
 SIGNUP_PATH_PATTERN = Regexp.new("\\A/(#{Rails.application.config.available_locales.keys.map { |locale| Regexp.escape(locale) }.join('|')})/users\\z")
 LOGIN_PATH_PATTERN = Regexp.new("\\A/(#{Rails.application.config.available_locales.keys.map { |locale| Regexp.escape(locale) }.join('|')})/users/sign_in\\z")
