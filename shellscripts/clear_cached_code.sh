7d6beb7d0bf4d3619c6cb53b4c79a229f049cec8
Jason Barnabe <jason.barnabe@gmail.com>
2023-11-21 18:59:47 -0600

    Clear cached code after 24h regardless of whether there was an update

--- shellscripts/clear_cached_code.sh
+++ shellscripts/clear_cached_code.sh
@@ -7,3 +7,7 @@
 echo "select id, DATE_SUB(code_updated_at, INTERVAL 1 SECOND) from scripts where code_updated_at > DATE_SUB(NOW(), INTERVAL 5 MINUTE);" | \
 mysql --skip-column-names | \
 awk 'BEGIN {FS="\t"};{ system("find /www/greasyfork/shared/tmp/cached_code/*/latest/scripts/" $1 ".* -type f ! -newermt \"" $2 " UTC\" -delete") }' 2> /dev/null
+
+# Delete anything with a ctime (which is create time for our purposes) over a day old, just in case we missed something
+# above, and also to clear out any deleted or rarely-accessed files.
+find /www/greasyfork/shared/public/cached_code/ -cmin +1440 -type f -delete 2>/dev/null
