b07ce6fcb9b5c7b2828c2207f14607834e3c3ea6
Jason Barnabe <jason.barnabe@gmail.com>
2024-01-19 16:13:29 -0600

    Enable Rails 7.1 defaults by default

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ Bundler.require(*Rails.groups)
 module Greasyfork
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     config.active_support.cache_format_version = 7.1
 
