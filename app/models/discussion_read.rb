6d44633c6e080b725e40594b7f88f53248425e5a
Jason Barnabe <jason.barnabe@gmail.com>
2024-01-25 19:19:58 -0600

    Fix discussion search for user who has marked all as read at some point

--- app/models/discussion_read.rb
+++ app/models/discussion_read.rb
@@ -3,11 +3,21 @@ class DiscussionRead < ApplicationRecord
   belongs_to :user
 
   def self.read_ids_for(discussions, user)
+    # Manually read discussions
     ids = where(user:, discussion_id: discussions.pluck(:id))
           .left_joins(:discussion)
           .where('discussions.stat_last_reply_date <= discussion_reads.read_at')
           .pluck(:discussion_id)
-    ids += discussions.where('discussions.stat_last_reply_date <= ?', user.discussions_read_since).pluck(:id) if user.discussions_read_since
+
+    # "Mark all as read" discussions
+    if user.discussions_read_since
+      ids += if discussions.is_a?(Array)
+               discussions.select { |d| d.stat_last_reply_date <= user.discussions_read_since }.map(&:id)
+             else
+               discussions.where('discussions.stat_last_reply_date <= ?', user.discussions_read_since).pluck(:id)
+             end
+    end
+
     ids.to_set
   end
 end
