e9bf0a1536affe6c66a136b62a87f4d7bccd45ea
Jason Barnabe <jason.barnabe@gmail.com>
2024-07-16 18:25:32 -0500

    Removed unused public DB scripts

--- shellscripts/create_public_db.sh
+++
@@ -1,2 +0,0 @@
-mysql --login-path=greasyfork < $(dirname $0)/create_public_db.sql
-mysqldump --login-path=greasyfork --single-transaction --quick greasyfork_public | gzip > $(dirname $0)/../public/data/db.sql.gz
