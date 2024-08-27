c0395f59df1d3a40e6af75bae91fae7661782240
Jason Barnabe <jason.barnabe@gmail.com>
2024-08-27 15:11:36 -0500

    Enable Rails 7.2 default for active_storage.web_image_content_types

--- config/initializers/new_framework_defaults_7_2.rb
+++ config/initializers/new_framework_defaults_7_2.rb
@@ -40,7 +40,7 @@ Rails.application.config.active_job.enqueue_after_transaction_commit = :default
 # This is possible due to broad browser support for WebP, but older browsers and email clients may still not support
 # WebP. Requires imagemagick/libvips built with WebP support.
 #++
-# Rails.application.config.active_storage.web_image_content_types = %w[image/png image/jpeg image/gif image/webp]
+Rails.application.config.active_storage.web_image_content_types = %w[image/png image/jpeg image/gif image/webp]
 
 ###
 # Enable validation of migration timestamps. When set, an ActiveRecord::InvalidMigrationTimestampError
