32689012fcead1dfe2d537e3106013142456b45e
Jason Barnabe <jason.barnabe@gmail.com>
2024-10-07 09:44:41 -0500

    Fix rubocop warning in request debugger

--- app/controllers/concerns/request_debugging.rb
+++ app/controllers/concerns/request_debugging.rb
@@ -2,6 +2,6 @@ module RequestDebugging
   extend ActiveSupport::Concern
 
   def log_headers
-    Rails.root.join('log/debug_headers.txt').write(request.url + "\n" + DateTime.now.to_s + "\n" + request.headers.select { |key, _val| key.starts_with?('HTTP_') }.map { |key, val| "#{key}: #{val}" }.join("\n") + "\n\n", mode: 'a+')
+    Rails.root.join('log/debug_headers.txt').write("#{request.url}\n#{DateTime.now}\n#{request.headers.select { |key, _val| key.starts_with?('HTTP_') }.map { |key, val| "#{key}: #{val}" }.join("\n")}\n\n", mode: 'a+')
   end
 end
