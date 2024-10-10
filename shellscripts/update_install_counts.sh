a58287d49d1a5a73d15273528c90b4c149fd0162
Jason Barnabe <jason.barnabe@gmail.com>
2024-10-09 20:22:53 -0500

    Reindex all users after update install count
    To catch any failures to update

--- shellscripts/update_install_counts.sh
+++ shellscripts/update_install_counts.sh
@@ -7,5 +7,6 @@ mysql < ./update_install_counts.sql >> ../log/update_install_counts.log 2>&1
 echo "Reindexing scripts at `date`" >> ../log/update_install_counts.log
 RAILS_ENV=production CLASS=Script /home/deploy/.rbenv/bin/rbenv exec bundle exec rails runner 'Script.reindex' >> ../log/update_install_counts.log 2>&1
 echo "Reindexing users at `date`" >> ../log/update_install_counts.log
-RAILS_ENV=production CLASS=User /home/deploy/.rbenv/bin/rbenv exec bundle exec rails runner 'User.script_authors.reindex' >> ../log/update_install_counts.log 2>&1
+# We could just do User.script_authors.reindex but we should reindex everyone in case other updates got missed.
+RAILS_ENV=production CLASS=User /home/deploy/.rbenv/bin/rbenv exec bundle exec rails runner 'User.reindex' >> ../log/update_install_counts.log 2>&1
 echo "Done at `date`" >> ../log/update_install_counts.log
