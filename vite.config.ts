1a5807c54fcf958b5c6451651fa55c3b674c2a50
Jason Barnabe <jason.barnabe@gmail.com>
2024-11-01 09:39:53 -0500

    Enable brotli compression in vite
    Switching from vite-plugin-compression to vite-plugin-compression2 as the former seems abandoned.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
-import viteCompression from 'vite-plugin-compression'
+import { compression } from 'vite-plugin-compression2'
 
 export default defineConfig({
   plugins: [
-    RubyPlugin(), viteCompression()
+    RubyPlugin(),
+    compression(),
+    compression({ algorithm: 'brotliCompress' })
   ],
 })
