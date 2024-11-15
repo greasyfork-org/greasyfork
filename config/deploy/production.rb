f636295761730ee2630cce11b96029e056373c6f
Jason Barnabe <jason.barnabe@gmail.com>
2024-11-15 11:31:57 -0600

    Use hostnames instead of IPs in deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '192.53.127.123', user: 'deploy', roles: %w[app db web]
-server '45.56.87.231', user: 'deploy', roles: %w[app web]
-server '45.79.87.224', user: 'deploy', roles: %w[app web]
-server '192.155.83.16', user: 'deploy', roles: %w[worker]
+server 'sisko', user: 'deploy', roles: %w[app db web]
+server 'janeway', user: 'deploy', roles: %w[app web]
+server 'archer', user: 'deploy', roles: %w[app web]
+server 'riker', user: 'deploy', roles: %w[worker]
