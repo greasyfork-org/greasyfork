03c1dc8a3d9d0c9325958aeaecd8d9c3e4611ab9
Jason Barnabe <jason.barnabe@gmail.com>
2024-11-15 09:33:15 -0600

    Do not send conversation notification emails if either user is banned

--- app/mailers/conversation_mailer.rb
+++ app/mailers/conversation_mailer.rb
@@ -4,8 +4,9 @@ class ConversationMailer < ApplicationMailer
   helper UserTextHelper
 
   def new_conversation(conversation, receiving_user, initiator_user)
-    # If the either user is deleted, then who cares.
+    # If the either user is deleted/banned, then who cares.
     return unless receiving_user && initiator_user
+    return if receiving_user.banned? || initiator_user.banned?
 
     @message = conversation.messages.first
     @receiving_user = receiving_user
@@ -24,6 +25,7 @@ class ConversationMailer < ApplicationMailer
   def new_message(message, receiving_user)
     # If the either user is deleted, then who cares.
     return unless receiving_user && message.poster
+    return if receiving_user.banned? || message.poster.banned?
 
     @message = message
     @receiving_user = receiving_user
