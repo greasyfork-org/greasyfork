7a8acb48099c2f097fef140136e0ebfa3f6f0b6d
Jason Barnabe <jason.barnabe@gmail.com>
2024-12-06 09:48:15 -0600

    Fix exponeniation in stat ban

--- app/models/stat_ban.rb
+++ app/models/stat_ban.rb
@@ -5,7 +5,7 @@ class StatBan < ApplicationRecord
 
   def self.add_next_ban!(script_id)
     previous_bans = StatBan.where(script_id:).count
-    ban_length_days = 2 ^ (previous_bans + 1)
+    ban_length_days = 2**(previous_bans + 1)
     Rails.logger.info("Banning script #{script_id} for #{ban_length_days} days")
     StatBan.create!(script_id:, expires_at: ban_length_days.days.from_now)
   end
