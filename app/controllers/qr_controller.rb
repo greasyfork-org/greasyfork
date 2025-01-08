116a44bf145ba038a9fe43c808c5cef31168d670
Jason Barnabe <jason.barnabe@gmail.com>
2025-01-08 16:23:19 -0600

    Add check for missing URL in QR code controller

--- app/controllers/qr_controller.rb
+++ app/controllers/qr_controller.rb
@@ -4,6 +4,11 @@ class QrController < ApplicationController
   skip_before_action :set_locale
 
   def show
+    unless params[:url]
+      head :not_found, content_type: 'text/plain'
+      return
+    end
+
     qrcode = RQRCode::QRCode.new(params[:url])
     render body: qrcode.as_svg, content_type: 'image/svg+xml'
   end
