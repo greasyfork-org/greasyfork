f106a5d478b836a50525add1a0bf7556665c19b7
Jason Barnabe <jason.barnabe@gmail.com>
2025-01-08 20:43:23 -0600

    Load cd when ea script fails to load

--- app/javascript/ethical-ads.js
+++ app/javascript/ethical-ads.js
@@ -12,37 +12,40 @@ function setupEthicalAdsFallback() {
       });
     }
 
-    if (placements.length > 0) {
-      return
+    if (placements.length == 0) {
+      switchToEaFallback()
     }
-
-    let placeholder = document.querySelector(".ea-fallback")
-    if (!placeholder) {
-      return
-    }
-    placeholder.classList.remove('ea-fallback')
-
-    let element = document.createElement(placeholder.getAttribute('data-element-name'))
-    placeholder.removeAttribute('data-element-name')
-
-    Array.from(placeholder.attributes).forEach((attr) => element.setAttributeNode(attr.cloneNode(true)))
-
-    let parent = placeholder.parentNode
-
-    if (placeholder.dataset['parentId']) {
-      let createdParent = document.createElement('div')
-      createdParent.id = placeholder.dataset['parentId']
-      createdParent.appendChild(element)
-      element = createdParent
-    }
-
-    parent.insertBefore(element, placeholder)
-    parent.removeChild(placeholder)
-    parent.style.display = "block"
-
-    let ethicalAdsElement = document.querySelector(".ethical-ads")
-    ethicalAdsElement.parentNode.removeChild(ethicalAdsElement)
   })
 }
+
+const switchToEaFallback = function() {
+  let placeholder = document.querySelector(".ea-fallback")
+  if (!placeholder) {
+    return
+  }
+  placeholder.classList.remove('ea-fallback')
+
+  let element = document.createElement(placeholder.getAttribute('data-element-name'))
+  placeholder.removeAttribute('data-element-name')
+
+  Array.from(placeholder.attributes).forEach((attr) => element.setAttributeNode(attr.cloneNode(true)))
+
+  let parent = placeholder.parentNode
+
+  if (placeholder.dataset['parentId']) {
+    let createdParent = document.createElement('div')
+    createdParent.id = placeholder.dataset['parentId']
+    createdParent.appendChild(element)
+    element = createdParent
+  }
+
+  parent.insertBefore(element, placeholder)
+  parent.removeChild(placeholder)
+  parent.style.display = "block"
+
+  let ethicalAdsElement = document.querySelector(".ethical-ads")
+  ethicalAdsElement.parentNode.removeChild(ethicalAdsElement)
+}
+window.switchToEaFallback = switchToEaFallback
 
 onload(setupEthicalAdsFallback)
