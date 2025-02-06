ee8b2e59fb9655da7945663b97dfecc1de56e880
Jason Barnabe <jason.barnabe@gmail.com>
2025-02-06 09:23:02 -0600

    Enable Devise extend_remember_period setting

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -131,7 +131,7 @@ Devise.setup do |config|
   # config.remember_for = 2.weeks
 
   # If true, extends the user's remember period when remembered via cookie.
-  # config.extend_remember_period = false
+  config.extend_remember_period = true
 
   # Options to be passed to the created cookie. For instance, you can set
   # :secure => true in order to force SSL only cookies.
