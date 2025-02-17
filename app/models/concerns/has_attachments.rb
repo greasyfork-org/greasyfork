2e9576c98ce85415b9712808057ea8ce7daa2930
Jason Barnabe <jason.barnabe@gmail.com>
2025-02-17 12:20:14 -0600

    Update active_storage_validations

--- app/models/concerns/has_attachments.rb
+++ app/models/concerns/has_attachments.rb
@@ -3,7 +3,7 @@ require 'active_support/concern'
 module HasAttachments
   extend ActiveSupport::Concern
 
-  ALLOWED_CONTENT_TYPES = %w[image/png image/jpg image/jpeg image/gif image/apng image/webp].freeze
+  ALLOWED_CONTENT_TYPES = %w[image/png image/jpeg image/gif image/webp].freeze
 
   included do
     has_many_attached :attachments
@@ -12,6 +12,6 @@ module HasAttachments
               content_type: ALLOWED_CONTENT_TYPES,
               size: { less_than: Rails.configuration.screenshot_max_size },
               limit: { max: Rails.configuration.screenshot_max_count },
-              processable_image: true
+              processable_file: true
   end
 end
