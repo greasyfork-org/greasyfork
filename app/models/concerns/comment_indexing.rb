1c1bc37b861993fea665e79d42d44f41281a4337
Jason Barnabe <jason.barnabe@gmail.com>
2025-03-19 19:46:22 -0500

    Fix error on paginating really far into comments

--- app/models/concerns/comment_indexing.rb
+++ app/models/concerns/comment_indexing.rb
@@ -3,6 +3,7 @@ module CommentIndexing
 
   included do
     searchkick callbacks: :async,
+               max_result_window: 10_000, # Refuse to load past this, as ES raises an error anyway
                filterable: [:discussion_category, :script_id, :discussion_id, :discussion_starter_id, :locale_id, :poster_id]
 
     scope :search_import, -> { includes(discussion: :script) }
