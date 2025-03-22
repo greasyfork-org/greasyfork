c9bdcf0b56b8f48f5961d3385880b3c03fa5df23
Jason Barnabe <jason.barnabe@gmail.com>
2025-03-22 10:53:44 -0500

    Fix exception when switching between .tld and not

--- app/models/script_applies_to.rb
+++ app/models/script_applies_to.rb
@@ -5,6 +5,14 @@ class ScriptAppliesTo < ApplicationRecord
   delegate :text, :domain_text, :domain?, to: :site_application
 
   after_destroy do
-    site_application.destroy if ScriptAppliesTo.where(site_application_id:).none?
+    # Destroy the site_application if it's not being used. Check for:
+    # 1. New ScriptAppliesTo on this script that will be using it.
+    # 2. ScriptAppliesTos on other scripts.
+    site_application.destroy if script.script_applies_tos.reject(&:marked_for_destruction?).none? { |sat| sat != self && sat.site_application == site_application } && ScriptAppliesTo.where(site_application_id:).none?
+  end
+
+  after_commit do
+    # site_applications is a has_many through this, and it doesn't seem to update when this is changed.
+    script.site_applications.reset if association(:script).loaded?
   end
 end
--- test/models/script_applies_to_test.rb
+++ test/models/script_applies_to_test.rb
@@ -48,6 +48,7 @@ class ScriptAppliesToTest < ActiveSupport::TestCase
     script.save!
 
     assert_equal 1, script.script_applies_tos.count
+    assert_equal 1, script.site_applications.count
     assert_equal 'example.com', script.site_applications.first.text
     assert_equal 'example.com', script.site_applications.first.domain_text
     assert script.site_applications.first.domain?
@@ -72,8 +73,231 @@ class ScriptAppliesToTest < ActiveSupport::TestCase
     script.save!
 
     assert_equal 1, script.script_applies_tos.count
+    assert_equal 1, script.site_applications.count
     assert_equal 'example.com', script.site_applications.first.text
     assert_nil script.site_applications.first.domain_text
     assert_not script.site_applications.first.domain?
+  end
+
+  test 'switching from .tld to .com' do
+    script = scripts(:example_com_application)
+
+    js = <<~JS
+      // ==UserScript==
+      // @name		A Test!
+      // @description		Unit test.
+      // @updateURL		http://example.com
+      // @namespace		http://greasyfork.local/users/1
+      // @match *://example.tld/*
+      // @license MIT
+      // ==/UserScript==
+      foo.baz();
+    JS
+    sv = ScriptVersion.new
+    sv.code = js
+    sv.version = '123'
+    sv.script = script
+    sv.rewritten_code = sv.calculate_rewritten_code
+    script.apply_from_script_version(sv)
+    script.save!
+
+    assert_equal 5, script.script_applies_tos.count
+    assert_includes script.site_applications.map(&:text), 'example.com'
+    assert_includes script.site_applications.map(&:text), 'example.net'
+    assert_includes script.site_applications.map(&:domain_text), 'example.com'
+    assert_includes script.site_applications.map(&:domain_text), 'example.net'
+    assert script.site_applications.all?(&:domain?)
+    assert_not script.script_applies_tos.find { |sat| sat.text == 'example.com' }.tld_extra
+    assert script.script_applies_tos.select { |sat| sat.text != 'example.com' }.all?(&:tld_extra?)
+
+    js = <<~JS
+      // ==UserScript==
+      // @name		A Test!
+      // @description		Unit test.
+      // @updateURL		http://example.com
+      // @namespace		http://greasyfork.local/users/1
+      // @match *://example.com/*
+      // @license MIT
+      // ==/UserScript==
+      foo.baz();
+    JS
+    sv = ScriptVersion.new
+    sv.code = js
+    sv.version = '123'
+    sv.script = script
+    sv.rewritten_code = sv.calculate_rewritten_code
+    script.apply_from_script_version(sv)
+    script.save!
+
+    assert_equal 1, script.script_applies_tos.count
+    assert_equal 1, script.site_applications.count
+    assert_equal 'example.com', script.site_applications.first.text
+    assert_not script.script_applies_tos.first.tld_extra?
+  end
+
+  test 'switching from .com to .tld' do
+    script = scripts(:example_com_application)
+
+    js = <<~JS
+      // ==UserScript==
+      // @name		A Test!
+      // @description		Unit test.
+      // @updateURL		http://example.com
+      // @namespace		http://greasyfork.local/users/1
+      // @match *://example.com/*
+      // @license MIT
+      // ==/UserScript==
+      foo.baz();
+    JS
+    sv = ScriptVersion.new
+    sv.code = js
+    sv.version = '123'
+    sv.script = script
+    sv.rewritten_code = sv.calculate_rewritten_code
+    script.apply_from_script_version(sv)
+    script.save!
+
+    assert_equal 1, script.script_applies_tos.count
+    assert_equal 1, script.site_applications.count
+    assert_equal 'example.com', script.site_applications.first.text
+    assert_not script.script_applies_tos.first.tld_extra?
+
+    js = <<~JS
+      // ==UserScript==
+      // @name		A Test!
+      // @description		Unit test.
+      // @updateURL		http://example.com
+      // @namespace		http://greasyfork.local/users/1
+      // @match *://example.tld/*
+      // @license MIT
+      // ==/UserScript==
+      foo.baz();
+    JS
+    sv = ScriptVersion.new
+    sv.code = js
+    sv.version = '123'
+    sv.script = script
+    sv.rewritten_code = sv.calculate_rewritten_code
+    script.apply_from_script_version(sv)
+    script.save!
+
+    assert_equal 5, script.script_applies_tos.count
+    assert_includes script.site_applications.map(&:text), 'example.com'
+    assert_includes script.site_applications.map(&:text), 'example.net'
+    assert_includes script.site_applications.map(&:domain_text), 'example.com'
+    assert_includes script.site_applications.map(&:domain_text), 'example.net'
+    assert_not script.script_applies_tos.find { |sat| sat.text == 'example.com' }.tld_extra
+    assert script.script_applies_tos.select { |sat| sat.text != 'example.com' }.all?(&:tld_extra?)
+  end
+
+  test 'switching from .tld to .net' do
+    script = scripts(:example_com_application)
+
+    js = <<~JS
+      // ==UserScript==
+      // @name		A Test!
+      // @description		Unit test.
+      // @updateURL		http://example.com
+      // @namespace		http://greasyfork.local/users/1
+      // @match *://example.tld/*
+      // @license MIT
+      // ==/UserScript==
+      foo.baz();
+    JS
+    sv = ScriptVersion.new
+    sv.code = js
+    sv.version = '123'
+    sv.script = script
+    sv.rewritten_code = sv.calculate_rewritten_code
+    script.apply_from_script_version(sv)
+    script.save!
+
+    assert_equal 5, script.script_applies_tos.count
+    assert_includes script.site_applications.map(&:text), 'example.com'
+    assert_includes script.site_applications.map(&:text), 'example.net'
+    assert_includes script.site_applications.map(&:domain_text), 'example.com'
+    assert_includes script.site_applications.map(&:domain_text), 'example.net'
+    assert script.site_applications.all?(&:domain?)
+    assert_not script.script_applies_tos.find { |sat| sat.text == 'example.com' }.tld_extra
+    assert script.script_applies_tos.select { |sat| sat.text != 'example.com' }.all?(&:tld_extra?)
+
+    js = <<~JS
+      // ==UserScript==
+      // @name		A Test!
+      // @description		Unit test.
+      // @updateURL		http://example.com
+      // @namespace		http://greasyfork.local/users/1
+      // @match *://example.net/*
+      // @license MIT
+      // ==/UserScript==
+      foo.baz();
+    JS
+    sv = ScriptVersion.new
+    sv.code = js
+    sv.version = '123'
+    sv.script = script
+    sv.rewritten_code = sv.calculate_rewritten_code
+    script.apply_from_script_version(sv)
+    script.save!
+
+    assert_equal 1, script.script_applies_tos.count
+    assert_equal 1, script.site_applications.count
+    assert_equal 'example.net', script.site_applications.first.text
+    assert_not script.script_applies_tos.first.tld_extra?
+  end
+
+  test 'switching from .net to .tld' do
+    script = scripts(:example_com_application)
+
+    js = <<~JS
+      // ==UserScript==
+      // @name		A Test!
+      // @description		Unit test.
+      // @updateURL		http://example.com
+      // @namespace		http://greasyfork.local/users/1
+      // @match *://example.net/*
+      // @license MIT
+      // ==/UserScript==
+      foo.baz();
+    JS
+    sv = ScriptVersion.new
+    sv.code = js
+    sv.version = '123'
+    sv.script = script
+    sv.rewritten_code = sv.calculate_rewritten_code
+    script.apply_from_script_version(sv)
+    script.save!
+
+    assert_equal 1, script.script_applies_tos.count
+    assert_equal 1, script.site_applications.count
+    assert_equal 'example.net', script.site_applications.first.text
+    assert_not script.script_applies_tos.first.tld_extra?
+
+    js = <<~JS
+      // ==UserScript==
+      // @name		A Test!
+      // @description		Unit test.
+      // @updateURL		http://example.com
+      // @namespace		http://greasyfork.local/users/1
+      // @match *://example.tld/*
+      // @license MIT
+      // ==/UserScript==
+      foo.baz();
+    JS
+    sv = ScriptVersion.new
+    sv.code = js
+    sv.version = '123'
+    sv.script = script
+    sv.rewritten_code = sv.calculate_rewritten_code
+    script.apply_from_script_version(sv)
+    script.save!
+
+    assert_equal 5, script.script_applies_tos.count
+    assert_includes script.site_applications.map(&:text), 'example.com'
+    assert_includes script.site_applications.map(&:text), 'example.net'
+    assert_includes script.site_applications.map(&:domain_text), 'example.com'
+    assert_includes script.site_applications.map(&:domain_text), 'example.net'
+    assert_not script.script_applies_tos.find { |sat| sat.text == 'example.com' }.tld_extra
+    assert script.script_applies_tos.select { |sat| sat.text != 'example.com' }.all?(&:tld_extra?)
   end
 end
