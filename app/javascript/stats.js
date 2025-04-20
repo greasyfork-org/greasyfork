90cdc151d4c6dde6961c120d522ce0796e4c90ce
Jason Barnabe <jason.barnabe@gmail.com>
2025-04-20 09:06:35 -0500

    Fix charts in dark mode
    Fixes #1412

--- app/javascript/stats.js
+++ app/javascript/stats.js
@@ -1,21 +1,23 @@
 /* eslint no-console:0 */
 
 window.initializeChart = async function(rawData, containerId) {
-  let data = {
-    labels: Object.keys(rawData),
-    datasets: [{
-      fillColor : "rgba(220,220,220,0.5)",
-      strokeColor : "rgba(220,220,220,1)",
-      data: Object.values(rawData)
-    }]
-  }
-
   let container = document.getElementById(containerId);
   let canvas = document.createElement("canvas")
   canvas.id = containerId + "-canvas";
   canvas.width = parseInt(getComputedStyle(container).width.replace("px", ""), 10) - 5;
   canvas.height = 400;
   container.appendChild(canvas)
+
+
+  console.log(getComputedStyle(canvas).getPropertyValue("--chart-fill-color"))
+  let data = {
+    labels: Object.keys(rawData),
+    datasets: [{
+      backgroundColor: getComputedStyle(canvas).getPropertyValue("--chart-background-color"),
+      borderColor: getComputedStyle(canvas).getPropertyValue("--chart-border-color"),
+      data: Object.values(rawData)
+    }]
+  }
 
   let ctx = canvas.getContext("2d");
   const { Chart, CategoryScale, LinearScale, BarController, BarElement, Tooltip } = await import('chart.js')
