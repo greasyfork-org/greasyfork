9c79f9e9164bac1d530b0d83507cbc3d818257df
Jason Barnabe <jason.barnabe@gmail.com>
2025-06-05 09:47:55 -0500

    Add a comment check word

--- app/services/comment_checking/custom_checker.rb
+++ app/services/comment_checking/custom_checker.rb
@@ -9,7 +9,8 @@ module CommentChecking
        'Keto ',
        'hbyvipxnzj.buzz',
        'gmkm.zrnq.one',
-       'Cocaine'].each do |snippet|
+       'Cocaine',
+       'Coinbase'].each do |snippet|
         return CommentChecking::Result.new(true, text: "Matched custom check for '#{snippet}'.") if comment.text.include?(snippet)
       end
 
