5af474525c41799c9e9ea79d975a0c571d85ec94
Jason Barnabe <jason.barnabe@gmail.com>
2025-07-11 08:45:29 -0500

    Adjust Sentry sampling rate

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,7 @@ Sentry.init do |config|
   config.dsn = Rails.application.credentials.sentry_api_key
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.excluded_exceptions += ['JSON::ParserError', 'Sidekiq::JobRetry::Skip', 'Sidekiq::Shutdown', 'Puma::HttpParserError', 'ActionDispatch::RemoteIp::IpSpoofAttackError', 'ActiveStorage::FileNotFoundError']
-  config.traces_sample_rate = 0.001
+  config.traces_sample_rate = 0.0007
   config.profiles_sample_rate = 0.001
   config.metrics.enabled = true
 
