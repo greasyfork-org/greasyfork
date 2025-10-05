c6d45e3e4835c420a0e3a7e13bd60c7b4ee19479
Jason Barnabe <jason.barnabe@gmail.com>
2025-10-04 19:44:52 -0500

    Add test for syncing libraries

--- test/integration/script_syncer_test.rb
+++ test/integration/script_syncer_test.rb
@@ -171,4 +171,16 @@ class ScriptSyncerTest < ActiveSupport::TestCase
     assert_equal 1, script.script_versions.first.attachments.length
     assert_equal 1, script.script_versions.last.attachments.length
   end
+
+  test 'sync library' do
+    script = scripts(:library)
+    assert_equal 1, script.script_versions.length
+    assert_equal :success, ScriptSyncer.sync(script), script.reload.sync_error
+    assert_equal 'jQuery', script.name
+    assert_equal Time.now.utc.to_date, script.code_updated_at.to_date
+    assert_equal Time.now.utc.to_date, script.last_attempted_sync_date.to_date
+    assert_equal Time.now.utc.to_date, script.last_successful_sync_date.to_date
+    assert_nil script.sync_error
+    assert_equal 2, script.script_versions.length
+  end
 end
