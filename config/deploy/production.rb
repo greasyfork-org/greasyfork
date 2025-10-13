4599b1a0b390c2d81ca4a541007cc79c4bd1a763
Jason Barnabe <jason.barnabe@gmail.com>
2025-10-13 14:14:27 -0500

    Remove sisko from deploy script

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,3 @@
-server 'sisko', user: 'deploy', roles: %w[app db web]
 server 'janeway', user: 'deploy', roles: %w[app web]
 server 'archer', user: 'deploy', roles: %w[app web]
 server 'riker', user: 'deploy', roles: %w[worker]
