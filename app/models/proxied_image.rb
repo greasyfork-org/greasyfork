1f5df37a48f6e4285d4f37b587bbe14ed4d2616e
Jason Barnabe <jason.barnabe@gmail.com>
2026-02-07 14:48:29 -0600

    Basics are working now

--- app/models/proxied_image.rb
+++ app/models/proxied_image.rb
@@ -1,7 +1,11 @@
 class ProxiedImage < ApplicationRecord
   has_one_attached :image
 
-  SAFE_HOSTS = ['greasyfork.org', 'sleazyfork.org'].freeze
+  SAFE_HOSTS = ['greasyfork.org', 'sleazyfork.org', 'amazonaws.com', 'github.com', 'shields.io', 'gitlab.com', 'githubusercontent.com'].freeze
+
+  def self.uri_needs_to_be_proxied?(uri)
+    SAFE_HOSTS.none?{|safe_host| uri.host == safe_host || uri.host&.end_with?(".#{safe_host}") }
+  end
 
   def self.proxied_url_for_url(original_url)
     # Ensure it's something we would actually want to use.
@@ -11,7 +15,7 @@ class ProxiedImage < ApplicationRecord
       return nil
     end
 
-    return original_url if SAFE_HOSTS.include?(uri.host)
+    return original_url unless uri_needs_to_be_proxied?(uri)
 
     proxied_image = find_by(original_url:)
 
@@ -22,7 +26,7 @@ class ProxiedImage < ApplicationRecord
     end
 
     # If we have a successful proxy, return the proxied URL.
-    return url_for(image.blob) if proxied_image.success
+    return proxied_image.image.blob.url if proxied_image.success
 
     # If we attempted but failed to proxy, return nil and render nothing. We could return the original, but this would
     # by a bypass of the original intent of preventing tracking via third-party image hosts.
@@ -30,35 +34,33 @@ class ProxiedImage < ApplicationRecord
   end
 
   def self.store(original_url, refresh_if_exists: false)
-    proxied_image = if refresh_if_exists
-                      find_or_initialize_by(original_url:)
-                    else
-                      find_by(original_url:)
-                    end
+    proxied_image = find_by(original_url:)
+    return if proxied_image && !refresh_if_exists
 
-    return unless proxied_image
+    proxied_image ||= ProxiedImage.new(original_url:)
+    proxied_image.load_media
+  end
 
-    begin
-      uri = validate_url!(original_url)
-      downloaded_image = uri.read({ read_timeout: 10 })
-      content_type = downloaded_image.content_type
+  def load_media
+    uri = self.class.validate_url!(original_url)
+    downloaded_image = uri.open(read_timeout: 10)
+    content_type = downloaded_image.content_type
 
-      raise "Unsupported content type: #{content_type}" unless HasAttachments::ALLOWED_CONTENT_TYPES.include?(content_type)
+    raise "Unsupported content type: #{content_type}" unless HasAttachments::ALLOWED_CONTENT_TYPES.include?(content_type)
 
-      proxied_image.image.attach(
-        io: downloaded_image,
-        filename: File.basename(uri.path),
-        content_type: content_type
-      )
+    image.attach(
+      io: downloaded_image,
+      filename: File.basename(uri.path),
+      content_type: content_type
+    )
 
-      proxied_image.success = true
-      proxied_image.last_error = nil
-    rescue StandardError => e
-      proxied_image.success = false
-      proxied_image.last_error = e.message.truncate(500)
-    ensure
-      proxied_image.save!
-    end
+    self.success = true
+    self.last_error = nil
+  rescue StandardError => e
+    self.success = false
+    self.last_error = e.message.truncate(500)
+  ensure
+    save!
   end
 
   def self.validate_url!(url)
