594b56c8190c18a20cfe0287e83c23070f551592
Jason Barnabe <jason.barnabe@gmail.com>
2026-02-11 20:03:40 -0600

    Handle really long host names in proxied images

--- db/migrate/20260212015452_add_host_to_proxied_images.rb
+++ db/migrate/20260212015452_add_host_to_proxied_images.rb
@@ -1,9 +1,9 @@
 class AddHostToProxiedImages < ActiveRecord::Migration[8.1]
   def change
-    add_column :proxied_images, :original_host, :string, limit: 200, null: true
+    add_column :proxied_images, :original_host, :string, limit: 200, null: true unless column_exists?(:proxied_images, :original_host)
 
     ProxiedImage.find_each do |proxied_image|
-      proxied_image.update!(original_host: URI.parse(proxied_image.original_url).host)
+      proxied_image.update!(original_host: URI.parse(proxied_image.original_url).host.truncate(200, omission: nil))
     end
     
     change_column_null :proxied_images, :original_host, false
