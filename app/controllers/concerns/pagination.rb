1fdbfac3e334ea139afa26dc77889bb952efa2dc
Jason Barnabe <jason.barnabe@gmail.com>
2026-03-26 09:51:49 -0500

    Apply max results limit to ActiveRecord pagination
    - Prevents bots from going crazy following the links despite rel=nofollow
    - Speeds up the count query for large results
    - Complete avoids the count and offset queries when past the limit

--- app/controllers/concerns/pagination.rb
+++ app/controllers/concerns/pagination.rb
@@ -2,17 +2,35 @@ module Pagination
   extend ActiveSupport::Concern
   include Pagy::Method
 
+  MAX_PAGINATION_RESULT = 10_000
+
   included do
     helper_method :render_pagination, :multiple_pages?
   end
 
   def apply_pagination(relation, default_per_page: 50)
-    @pagy, relation = pagy(relation, limit: per_page(default: default_per_page), max_pages: 1000)
+    page = page_number
+    limit = per_page(default: default_per_page)
+
+    # Apply a limit to the number of pages we'll load, because bots seem to go nuts and paginate endlessly despite the directives
+    # telling them not to.
+    relation = if (page - 1) * limit >= MAX_PAGINATION_RESULT
+                 # If we're past the max result, show no results.
+                 relation.none
+               else
+                 # If we're before the max result, apply a limit to make it so we don't generate links past the max result. This also
+                 # speeds up the count query.
+                 relation.limit(MAX_PAGINATION_RESULT)
+               end
+
+    @pagy, relation = pagy(relation, page:, limit:)
     relation
   end
 
   def apply_searchkick_pagination(search)
-    @pagy = pagy(:searchkick, search, max_pages: 1000)
+    # In contrast to ActiveRecord relations above in apply_pagination, Searchkick has a built-in 10,000 result limit, so we don't need
+    # to apply out own limit.
+    @pagy = pagy(:searchkick, search)
     search
   end
 
@@ -31,7 +49,7 @@ module Pagination
   end
 
   def page_number
-    return nil unless params[:page].is_a?(String)
+    return 1 unless params[:page].is_a?(String)
 
     page = params[:page].to_i
     page = 1 if page.nil? || page < 1
