68f47fe958af4f0245f6cdf195845bdc45a82d1f
Jason Barnabe <jason.barnabe@gmail.com>
2026-05-25 22:16:06 -0500

    Add a source to determine if an IPv6 is a data centre

--- lib/data_centre_ips.rb
+++ lib/data_centre_ips.rb
@@ -1,24 +1,52 @@
 class DataCentreIps
   # This CSV has a range per row, with the starting IP as the first column and the ending IP as the second. It's
-  # non-overlapping and sorted.
-  DATA_SOURCE = URI.parse('https://raw.githubusercontent.com/growlfm/ipcat/refs/heads/main/datacenters.csv')
+  # non-overlapping and sorted. It only contains IPV4 addresses.
+  DATA_SOURCE_IPCAT = URI.parse('https://raw.githubusercontent.com/growlfm/ipcat/refs/heads/main/datacenters.csv')
 
-  def load_ip_ranges(force: false)
-    @load_ip_ranges ||= Rails.cache.fetch('data_centre_ips', force:, expires_in: 1.day) do
-      Net::HTTP.get(DATA_SOURCE)
+  # This CSV has a range per row and supports IPV4 and IPV6, but it's not limited to data centres. We need to manually
+  # identify data centres based on ASN.
+  DATA_SOURCE_DBIP = URI.parse('https://download.db-ip.com/free/dbip-asn-lite-2026-05.csv.gz')
+
+  DATA_CENTRE_ASN_NUMBERS = [
+    '18450', # WebNX, Inc.
+  ].freeze
+
+  def load_ip_ranges_ipcat(force: false)
+    @load_ip_ranges_ipcat ||= Rails.cache.fetch('data_centre_ips_ipcat', force:, expires_in: 1.day) do
+      Net::HTTP.get(DATA_SOURCE_IPCAT)
                .split("\n")
                .map { |row| row.split(',')[0..1].map { |ip| IPAddr.new(ip).to_i } }
     end
   end
 
+  def load_ip_ranges_dbip(force: false)
+    @load_ip_ranges_dbip ||= Rails.cache.fetch('data_centre_ips_dbip', force:, expires_in: 1.day) do
+      Zlib::GzipReader.wrap(DATA_SOURCE_DBIP.open) do |gz|
+        gz.read.each_line.filter_map do |line|
+          ip_start, ip_end, asn_number, _asn_name = line.split(',')
+          next unless DATA_CENTRE_ASN_NUMBERS.include?(asn_number)
+
+          [IPAddr.new(ip_start).to_i, IPAddr.new(ip_end).to_i]
+        end
+      end
+    end
+  end
+
   def data_centre?(ip)
     ip_int = IPAddr.new(ip).to_i
+    data_centre_according_to_ipcat?(ip_int) || data_centre_according_to_dbip?(ip_int)
+  end
 
+  def data_centre_according_to_ipcat?(ip_int)
     # Find the first one where we're before the end of the range.
-    first_range = load_ip_ranges.find { |range| ip_int <= range[1] }
+    first_range = load_ip_ranges_ipcat.find { |range| ip_int <= range[1] }
     return false unless first_range
 
     # Check the start of the range.
     ip_int >= first_range[0]
+  end
+
+  def data_centre_according_to_dbip?(ip_int)
+    load_ip_ranges_dbip.any? { |ip_start, ip_end| ip_int.between?(ip_start, ip_end) }
   end
 end
