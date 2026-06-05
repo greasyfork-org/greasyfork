fca2f882c4ad8584799c46648bcfeeeca663582b
Jason Barnabe <jason.barnabe@gmail.com>
2026-06-05 18:39:49 -0500

    Add method to analyze a list of IPs

--- lib/data_centre_ips.rb
+++ lib/data_centre_ips.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class DataCentreIps
   # This CSV has a range per row, with the starting IP as the first column and the ending IP as the second. It's
   # non-overlapping and sorted. It only contains IPV4 addresses.
@@ -51,5 +53,35 @@ class DataCentreIps
 
   def data_centre_according_to_dbip?(ip_int)
     load_ip_ranges_dbip.any? { |ip_start, ip_end| ip_int.between?(ip_start, ip_end) }
+  end
+
+  def analyze_ips_with_dbip(ips)
+    dbip_data = nil
+
+    Zlib::GzipReader.wrap(DATA_SOURCE_DBIP.open) do |gz|
+      dbip_data = gz.read.each_line.map do |line|
+        ip_start, ip_end, asn_number, asn_name = line.split(',')
+        [IPAddr.new(ip_start).to_i, IPAddr.new(ip_end).to_i, asn_number, asn_name]
+      end
+    end
+
+    ips.map do |ip|
+      ip_int = IPAddr.new(ip).to_i
+      matching_entry = dbip_data.find { |ip_start, ip_end, _asn_number, _asn_name| ip_int.between?(ip_start, ip_end) }
+
+      if matching_entry
+        {
+          ip: ip,
+          asn_number: matching_entry[2],
+          asn_name: matching_entry[3].strip,
+        }
+      else
+        {
+          ip: ip,
+          asn_number: nil,
+          asn_name: nil,
+        }
+      end
+    end
   end
 end
