9a1ee9215b21f012948115516d5fd56bc28b8c48
Jason Barnabe <jason.barnabe@gmail.com>
2026-06-22 15:07:52 -0500

    Catch Addressable::URI::InvalidURIError when absolutizing URLs

--- lib/script_importer/base_script_importer.rb
+++ lib/script_importer/base_script_importer.rb
@@ -118,10 +118,14 @@ module ScriptImporter
         next unless url_text
         next if url_text.starts_with?('#')
 
-        new_url = base_url.join(url_text)
-        if url_text != new_url.to_s
-          changed = true
-          node[url_param] = new_url
+        begin
+          new_url = base_url.join(url_text)
+          if url_text != new_url.to_s
+            changed = true
+            node[url_param] = new_url
+          end
+        rescue Addressable::URI::InvalidURIError
+          # Leave as is
         end
       end
       return html unless changed
