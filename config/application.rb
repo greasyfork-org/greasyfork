36f0f65f24f9379dbf7ae1a8f3f6714e8f0f76c6
Jason Barnabe <jason.barnabe@gmail.com>
2026-09-24 11:42:22 -0500

    Fix custom logging class namespace

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module Greasyfork
     config.exceptions_app = routes
 
     config.ip_address_tracking = true
-    config.middleware.insert_before 0, UserRequestLogger
+    config.middleware.insert_before 0, Middleware::UserRequestLogger
   end
 end
 
