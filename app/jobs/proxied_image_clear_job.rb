b70304134339463ee29326dcb5411ffc25c8406d
Jason Barnabe <jason.barnabe@gmail.com>
2026-09-26 17:26:44 -0500

    Drop proxied image code

--- app/jobs/proxied_image_clear_job.rb
+++
@@ -1,9 +0,0 @@
-class ProxiedImageClearJob
-  include Sidekiq::Job
-
-  sidekiq_options queue: 'background', lock: :until_executed, on_conflict: :log, lock_ttl: 15.minutes.to_i
-
-  def perform
-    ProxiedImage.expired.find_each(&:destroy)
-  end
-end
